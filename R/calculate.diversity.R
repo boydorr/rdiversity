e88697480d60750a81fd8951cf1541cd38ab36bc
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-02-24 22:36:03 +0000

    Edited code to utilise diversity output attributes rather than grep() Maintained readable format, albeit in list form - will revisit this later. Where each list item carries attributes from previous step :)

--- R/calculate.diversity.R
+++ R/calculate.diversity.R
@@ -1,36 +1,33 @@
-calculate.diversity <-
-function(measure, pmatrix, qs, zmatrix = diag(nrow(pmatrix))) 
-{ 
-  # If a single measure was entered, put it in a list
-  if(!is.list(measure)&length(measure)==1) measure <- list(measure)
-  if(!is.list(measure)&length(measure)!=1) 
-    stop('Measure(s) must be entered as a list, or as a single string')
-  
-  # Check measures belong to the partitioning framework
-  if(any(!unlist(lapply(measure,function(x) is(x,'diversity'))))) 
-    stop('Measure does not exist.')
-  
-  # Calculate diversity
-  output <- lapply(measure, function(x) {
-    tag <- attr(x,'name')
-    if(any(grep('subcommunity',tag))){
+diversity <-
+  function(measure, pmatrix, qs, zmatrix = diag(nrow(pmatrix))) 
+  {
+    output <- lapply(measure, function(x) {
+      
       ans <- x(pmatrix, qs, zmatrix)
-      ans <- reshape2::melt(as.matrix(ans))
-      ans <- cbind(ans, tag)
-      colnames(ans) <- c('subcommunity','q','diversity','measure')
-      ans$q <- as.numeric(gsub('q', '', ans$q))
-      row.names(ans) <- NULL
-      return(ans)
-    }else if(any(grep('supercommunity',tag))){
-      ans <- x(pmatrix, qs, zmatrix)
-      ans <- cbind.data.frame('all', row.names(ans), ans, tag)
-      colnames(ans) <- c('subcommunity','q','diversity','measure')
-      ans$q <- as.numeric(gsub('q', '', ans$q))
-      row.names(ans) <- NULL
-      return(ans)
-    }else stop('Something is wrong here..')
-  })
-  # Put everything in a nice data table
-  output <- do.call(rbind,output)
-  return(output)
+      tag <- attr(ans, 'measure') 
+      
+      if(attr(ans,'type')=='subcommunity') {
+        tmp <- reshape2::melt(as.matrix(ans)) 
+        tmp <- cbind(tmp, tag)
+        colnames(tmp) <- c('subcommunity','q','diversity','measure')
+        tmp$q <- as.numeric(gsub('q', '', tmp$q))
+        row.names(tmp) <- NULL
+        attr(tmp,'measure') <- attr(ans,'measure')
+        attr(tmp,'tag') <- attr(ans,'tag')
+        attr(tmp,'type') <- attr(ans,'type')
+        return(tmp)
+        
+    }else if(attr(ans,'type')=='supercommunity') {
+      tmp <- reshape2::melt(as.matrix(ans)) 
+      tmp <- cbind(tmp, tag)
+      tmp <- tmp[,-2]
+      colnames(tmp) <- c('q','diversity','measure')
+      tmp$q <- as.numeric(gsub('q', '', tmp$q))
+      row.names(tmp) <- NULL
+      attr(tmp,'measure') <- attr(ans,'measure')
+      attr(tmp,'tag') <- attr(ans,'tag')
+      attr(tmp,'type') <- attr(ans,'type')
+      return(tmp)
+    }}
+  )
 }
