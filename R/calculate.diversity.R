4fbe908b7906c2cf2f1767bf70796e149365932c
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-02-25 23:43:56 +0000

    Edited documentation for diversity() and changed arguments back to 'pmatrix' and 'zmatrix'

--- R/calculate.diversity.R
+++ R/calculate.diversity.R
@@ -1,20 +1,39 @@
 #' Calculate Diversity
 #' 
-#' Calculates the diversity, subcommunity or supercommunity, of a series of 
-#' columns representing independent subcommunity counts, for a series of 
-#' orders, repesented as a vector of qs.
-#'
-#' @param measure Diversity measure
-#' @param populations Population counts or proportions
-#' @param qs Vector of values of parameter q
-#' @param Z Similarity matrix
+#' The function \code{diversity} calculates the diversity of a supercommunity 
+#' or series of subcommunities inclusive of similarity. 
+#' 
+#' The argument \code{measure} takes the following inputs: \cr
+#' \tabular{ll}{
+#' \code{subcommunity.alpha} \tab - estimate of naive-community supercommunity diversity  \cr
+#' \code{subcommunity.alpha.bar} \tab - similarity-sensitive diversity of subcommunity \emph{j} in isolation \cr
+#' \code{subcommunity.rho} \tab - redundancy of subcommunity \emph{j} \cr
+#' \code{subcommunity.rho.bar} \tab - representativeness of subcommunity \emph{j}  \cr
+#' \code{subcommunity.beta} \tab - distinctiveness of subcommunity \emph{j} \cr
+#' \code{subcommunity.beta.bar} \tab - estimate of effective number of distinct subcommunities  \cr
+#' \code{subcommunity.gamma} \tab - contribution per individual toward supercommunity diversity \cr
+#' \code{supercommunity.A} \tab - naive-community supercommunity diversity \cr
+#' \code{supercommunity.A.bar} \tab - average similarity-sensitive diversity of subcommunities \cr
+#' \code{supercommunity.R} for \tab - average redundancy of subcommunities \cr
+#' \code{supercommunity.R.bar} \tab - average representativeness of subcommunities \cr
+#' \code{supercommunity.B} \tab - average distinctiveness of subcommunities \cr
+#' \code{supercommunity.B.bar} \tab - effective number of distinct subcommunities  \cr
+#' \code{supercommunity.G} \tab - supercommunity similarity-sensitive diversity \cr
+#' }
+#' 
+#' @param measure diversity measure (see Details)
+#' @param pmatrix matrix of population counts or proportions. 
+#' These are defined as a series of columns representing independent subcommunity counts
+#' @param qs vector of \emph{q} values. These are defined as the order of 
+#' diversity / parameter of conservativeness.
+#' @param zmatrix matrix of similarities between 'types' 
 #'
 #' @return A list of length = length(measure), where each list item contains 
 #' the diversity output for the corresponding measure
 #' @export
 #' 
 diversity <-
-  function(measure, populations, qs, zmatrix = diag(nrow(pmatrix))) 
+  function(measure, pmatrix, qs, zmatrix = diag(nrow(pmatrix))) 
   {
     output <- lapply(measure, function(x) {
       
