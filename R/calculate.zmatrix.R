fee5997fdcae27386d178a2dd2e884911bd58461
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-02-28 16:14:11 +0000

    Create zmatrix for type=='taxonomic'

--- R/calculate.zmatrix.R
+++ R/calculate.zmatrix.R
@@ -16,7 +16,7 @@
 #' four columns, listing 'Species', 'Genus', 'Family', and 'Subclass'
 #' @return matrix of pair-wise similarity indices
 #' 
-calculate.zmatrix <- function(type, data) 
+calculate.zmatrix <- function(type, data, lookup = NA) 
 {
   if(type='unique') {
     N <- nrow(data)
@@ -25,5 +25,34 @@ calculate.zmatrix <- function(type, data)
     colnames <- row.names(data)
     return(zmatrix)
     
+  }else if('taxonomic') {
+    if(nrow(data)!=nrow(lookup)) stop('data and lookup do not match up')
+    N <- nrow(data)
+    
+    # Based on Shimatani's taxonomic similarity indices
+    species.similarity <- 1
+    genus.similarity <- 0.75
+    family.similarity <- 0.5
+    subclass.similarity <- 0.25 
+    other.similarity <- 0
+    
+    zmatrix <- diag(N)
+    for (i in 1:nrow(lookup)) {
+      for (j in 1:nrow(lookup)) {
+        
+        if (lookup$Species[i]==lookup$Species[j]) {
+          zmatrix[i,j] <- species.similarity
+        } else if (lookup$Genus[i]==lookup$Genus[j]) {
+          zmatrix[i,j] <- genus.similarity
+        } else if (lookup$Family[i]==lookup$Family[j]) {
+          zmatrix[i,j] <- family.similarity
+        } else if (lookup$Subclass[i]==lookup$Subclass[j]) {
+          zmatrix[i,j] <- subclass.similarity
+        } else zmatrix[i,j] <- other.similarity
+      }
+    }
+    row.names <- row.names(data)
+    colnames <- row.names(data)
+    return(zmatrix)
   }
 }
