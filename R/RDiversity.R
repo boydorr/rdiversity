cfcd62b4721cbdd6b89bd36959e8afc453b09f4c
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-02-28 14:31:40 +0000

    Start general documentation for RDiversity package

---
+++ R/RDiversity.R
@@ -0,0 +1,18 @@
+#' RDiversity
+#' 
+#' The \code{RDiversity} package calculates diversity.
+#' 
+#' @details dfjsgj
+#' 
+#' @name RDiversity
+#' @docType package
+#' @import reshape2
+#' 
+#' @author 
+#' Sonia Mitchell,
+#' Richard Reeve
+#' \cr
+#' Maintainer: Sonia Mitchell 
+#' 
+#' 
+NULL
