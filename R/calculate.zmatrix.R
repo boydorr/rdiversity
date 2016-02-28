65f05507fb7ed9dd8995973c5448dc05cff15a49
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-02-28 16:12:13 +0000

    Edits to documentation

--- R/calculate.zmatrix.R
+++ R/calculate.zmatrix.R
@@ -2,15 +2,18 @@
 #' 
 #' The function \code{calculate.zmatrix} calculates the similarity matrix.
 #' 
-#' The argument \code{similarity} takes the following inputs: \cr
+#' The argument \code{type} takes the following inputs: \cr
 #' \tabular{ll}{
 #' \code{unique} \tab - assumes all \emph{types} are unique \cr
 #' \code{taxonomic} \tab - assumes all \emph{types} are species; calculates a 
 #' similarity matrix based on Shimatani's taxonomic similarity \cr
 #' }
 #' 
-#' @param 
-#' @param 
+#' @param type character string; defining which similarity measure is being 
+#' called for. 
+#' @param data matrix of count data
+#' @param lookup required for calculating \code{taxonomic} similarity; contains 
+#' four columns, listing 'Species', 'Genus', 'Family', and 'Subclass'
 #' @return matrix of pair-wise similarity indices
 #' 
 calculate.zmatrix <- function(similarity, data) 
