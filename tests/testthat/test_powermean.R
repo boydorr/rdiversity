b4f680084c227bfdacdad86d80011c7290e6fee0
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-02-28 14:27:32 +0000

    Tidy up comments

--- tests/testthat/test_powermean.R
+++ tests/testthat/test_powermean.R
@@ -7,11 +7,10 @@ test_that("power.mean returns the correct answers for a set of positive integers
   expect_equal(power.mean(numbers, 1), 6.2)
   expect_equal(power.mean(numbers, -Inf), 1)
   expect_equal(power.mean(numbers, Inf, c(1, 1, 1, 1, 0)), 8)
-  ## Check that a warning is returned when 'values' are incomplete, 
-  ## i.e. is.na(x) == T
+  # Check that a warning is returned when 'values' are incomplete, 
+  # i.e. is.na(x) == T
   expect_error(power.mean(c(NA,1)))
-  ## Check that a warning is returned when 'values' are input as negative numbers, 
-  ## when 'order' does not equal 2
+  # when 'order' does not equal 2
   expect_error(power.mean(c(-2,2),1))
   expect_equal(power.mean(c(-2,2),2), 2)
 })
