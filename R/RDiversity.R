e636258a344fa4da2fe5c3efb4c50f17956bf3ee
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-02-28 15:30:17 +0000

    Add comments to general RDiversity documentation

--- R/RDiversity.R
+++ R/RDiversity.R
@@ -1,18 +1,21 @@
 #' RDiversity
 #' 
-#' The \code{RDiversity} package calculates diversity.
+#' The \code{RDiversity} package calculates the similarity-sensitive diversity 
+#' of a population and it's partitions.
 #' 
-#' @details dfjsgj
+#' @details This package calculates, or takes as input, the similarity between 
+#' types; which may be unique, taxonimic, phenotypic, genetic, 
+#' phylogenetic, functional, and so on. A type is defined as the categorical 
+#' variable in which the similarity of individuals is assessed. 
 #' 
 #' @name RDiversity
 #' @docType package
 #' @import reshape2
 #' 
 #' @author 
-#' Sonia Mitchell,
-#' Richard Reeve
+#' Sonia Mitchell <soniamitchell@gmail.com>, Richard Reeve 
 #' \cr
-#' Maintainer: Sonia Mitchell 
+#' Maintainer: Sonia Mitchell <soniamitchell@gmail.com>
 #' 
 #' 
 NULL
