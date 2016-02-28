a9dc3b44917a21fdf1925649e52a7061e89b0a96
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-02-28 15:30:33 +0000

    Start new function to calculate similarity matrix

---
+++ R/calculate.zmatrix.R
@@ -0,0 +1,19 @@
+#' Calculate Similarity Matrix (Z)
+#' 
+#' The function \code{calculate.zmatrix} calculates the similarity matrix.
+#' 
+#' The argument \code{similarity} takes the following inputs: \cr
+#' \tabular{ll}{
+#' \code{unique} \tab - assumes all \emph{types} are unique \cr
+#' \code{taxonomic} \tab - assumes all \emph{types} are species; calculates a 
+#' similarity matrix based on Shimatani's taxonomic similarity \cr
+#' }
+#' 
+#' @param 
+#' @param 
+#' @return matrix of pair-wise similarity indices
+#' 
+calculate.zmatrix <- function(similarity, data) 
+{
+  
+}
