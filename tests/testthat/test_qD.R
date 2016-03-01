e314d43fba3c5a784cdf432d60e1303674b1b950
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-01 18:52:58 +0000

    Fix tests: re-include dirichlet distributions and re-balance tests to account for 'div', 'data.frame' style outputs

--- tests/testthat/test_qD.R
+++ tests/testthat/test_qD.R
@@ -1,36 +1,33 @@
 context('Testing the qD() function')
+require(gtools)
 
 numspecies <- 100
-fragments <- runif(numspecies)
-# weights <- t(MCMCpack::rdirichlet(1, rep(1, numspecies)))
-weights <- runif(numspecies)
-weights <- weights/sum(weights)
+weights <- t(rdirichlet(1, rep(1, numspecies)))
 numcommunities <- 8
-# manyweights <- t(rdirichlet(numcommunities, rep(1, numspecies)))
-manyweights <- matrix(runif(numcommunities*numspecies),ncol=numcommunities)
-manyweights <- apply(manyweights, 2, function(x) x/sum(x))
+manyweights <- t(rdirichlet(numcommunities, rep(1, numspecies)))
 
 test_that("The basic qD diversity calculations", {
-  expect_equal(qD(weights, 0), 
+  expect_equal(c(qD(weights, 0)), 
                sum(sapply(weights,function(x) all.equal(x,0)) != T))
-  expect_equal(vec(qD(weights, 1)), prod(weights ^ -weights))
-  expect_equal(vec(qD(weights, 2)), 1 / sum(weights ^ 2))
-  expect_equal(vec(qD(weights, Inf)), 1 / max(weights))
-  expect_equal(vec(qD(weights, c(1,2))), c(qD(weights, 1), qD(weights, 2))) 
+  expect_equal(c(qD(weights, 1)), prod(weights ^ -weights))
+  expect_equal(c(qD(weights, 2)), 1 / sum(weights ^ 2))
+  expect_equal(c(qD(weights, Inf)), 1 / max(weights))
+  expect_equal(c(qD(weights, c(1,2))), c(qD(weights, 1), qD(weights, 2))) 
 })
 
-expect_equivalent(qD(manyweights, 0), 
-                  t(numspecies * replicate(dim(manyweights)[2],1)))
-#' @test_approx_eq qD(manyweights, [0]) numspecies * ones((1, size(manyweights)[2]))
-#' @test_approx_eq qDZ(manyweights, [0, 1, 2, Inf],
-#'                     ones((size(manyweights)[1],
-#'                           size(manyweights)[1]))) ones((4, size(manyweights)[2]))
+test_that("qD diversity across multiple populations", {
+  expect_equivalent(qD(manyweights, 0), 
+                    t(numspecies * replicate(dim(manyweights)[2],1)))
+  expect_equivalent(qD(manyweights, c(0)), 
+                    t(numspecies * replicate(dim(manyweights)[2],1)))
+})
 
 
 context('Testing qD() warning generation')
+
 pop <- c(0.1, 0.1)
 
 test_that ("Generate warnings, but normalise and calculate diversities", {
-  expect_equivalent(qD(pop/sum(pop), 1), 2)
+  expect_equal(c(qD(pop/sum(pop), 1)), 2)
   expect_error(qD(pop, 1))
 })
--- tests/testthat/test_qDZ.R
+++ tests/testthat/test_qDZ.R
@@ -1,36 +1,28 @@
 context('The general Leinster-Cobbold calculations and qDZ()')
 
+numspecies <- 100
+weights <- t(rdirichlet(1, rep(1, numspecies)))
 Z1 <- matrix(rep(1, times = (length(weights))^2), 
              length(weights), length(weights))
-
-test_that ("The qDZ() function works for a single population", {       
-  expect_equal(y(qDZ(weights, c(1, 2))), y(qD(weights, c(1,2))))
-  expect_equal(y(qDZ(weights,c(0,1,2,3,Inf), Z1)), y(c(1,1,1,1,1)))
-})
-
-
 numcommunities <- 8
 manyweights <- t(rdirichlet(numcommunities, rep(1, numspecies)))
 
-test_that ("The qDZ() works for multiple populations ", {  
-  Z.tmp <- matrix(rep(1, length(manyweights[,1])^2), numspecies, numspecies) 
-  
-  # @test_approx_eq qD(manyweights, 0) numspecies * ones((1, size(manyweights)[2]))
-  # @test_approx_eq qD(manyweights, [0]) 
-  #           numspecies * ones((1, size(manyweights)[2]))    DON'T NEED!!
-  # @test_approx_eq qDZ(manyweights, [0, 1, 2, Inf], ones((size(manyweights)[1],
-  #           size(manyweights)[1]))) ones((4, size(manyweights)[2]))
-  expect_equal(y(qD(manyweights,0)), y(numspecies * rep(1,numcommunities)))
-  expect_equal(y(qDZ(manyweights, c(0, 1, 2, Inf), Z.tmp)), 
-               y(matrix(rep(1, length(numcommunities))^2,numcommunities,4)))
+test_that ("The qDZ() function works for a single population", {
+  expect_equivalent(qDZ(weights, c(1, 2)), c(qD(weights, c(1,2))))
+  expect_equivalent(qDZ(weights,c(0,1,2,3,Inf), Z1), c(1,1,1,1,1))
 })
 
 
-context('Testing qD() warning generation')
+test_that ("The qDZ() function works for a multiple populations", {       
+expect_equivalent(qDZ(manyweights, c(0, 1, 2, Inf),
+                 matrix(rep(1, numspecies*numspecies), ncol=numspecies)),
+                 data.frame(matrix(rep(1,(4 * dim(manyweights)[2])),ncol=4)))
+})
+
+context('Testing qDZ() warning generation')
+pop <- c(0.1, 0.1)
 
 test_that ("Generate warnings, but normalise and calculate diversities", {
-  #@test_approx_eq qD([0.1, 0.1], 1) 2.
-  #@test_approx_eq qDZ([0.1, 0.1], 1) 2.
-  expect_error(qD(c(0.1, 0.1), 1))
-  expect_error(qDZ(c(0.1, 0.1), 1))  
+  expect_equivalent(qDZ(pop/sum(pop), 1), 2)  
+  expect_error(qDZ(pop, 1))  
 })
