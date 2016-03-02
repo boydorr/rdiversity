7aa04bcb79e740532f3466437f05cfc14180a3ad
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-02 21:40:30 +0000

    define S4 generic plot() for class 'rdiv' - used to check whether ggplot2 was installed and plotting base graphics or ggplot graphics depending on the outcome, now only uses ggplot2

--- R/class-RDiversity.R
+++ R/class-RDiversity.R
@@ -22,37 +22,44 @@ is.RDiversity <-
   }
 
 
-setGeneric("plot.diversity", 
-           # valueClass = "gg", 
-           function(results) {
-             standardGeneric("plot.diversity")
 setMethod(f = "show", signature(object = "rdiv"), 
           definition = function(object){
             cat(object@measure, '\n\n')
             print(object)} )
+
+# Initialize generic function
+setGeneric(name = "plot", 
+           valueClass = "gg",
+           def = function(results, ...) {
+             standardGeneric("plot")
            })
 
-setMethod("plot.diversity", "rdiv", function(results) 
+# Define methods for generic 
+setMethod(f = "plot", signature = "rdiv", definition = function(results, style='normal') 
 {
-  if(is.element('ggplot2', installed.packages()[,1])) {
+  # if(is.element('ggplot2', installed.packages()[,1])) {
     plot.this <- cbind(stack(results), row.names(results), stringsAsFactors=F)
     colnames(plot.this) <- c('diversity', 'q', 'subcommunity')
     plot.this$q <- as.numeric(gsub('q', '', plot.this$q))
     
-    g <- ggplot2::ggplot() + theme_classic() 
-    g <- g + geom_line(aes(x = q, 
-                           y = diversity, 
-                           group = subcommunity,
-                           colour = subcommunity), data = plot.this) 
-    g <- g + labs(x = bquote(italic('q')), y = results@tag)
+    g <- ggplot2::ggplot() + ggplot2::theme_classic() 
+    g <- g + ggplot2::geom_line(ggplot2::aes_string(x = 'q', 
+                           y = 'diversity', 
+                           group = 'subcommunity',
+                           colour = 'subcommunity'), data = plot.this) 
+    g <- g + ggplot2::labs(x = bquote(italic('q')), y = results@tag)
+    
+    if(style=='big') {
+      g <- g + ggplot2::theme(text = ggplot2::element_text(size=20))
+    }
     g
-  } else {
-    plot.this <- t(data.frame(results))
-    g <- matplot(plot.this, type='l', col = 1:nrow(results),
-                 xlab = bquote(italic('q')), 
-                 ylab = results@tag)
+    
+  # } else {
+  #   plot.this <- t(data.frame(results))
+  #   g <- matplot(plot.this, type='l', col = 1:nrow(results),
+  #                xlab = bquote(italic('q')), 
+  #                ylab = results@tag)
     # legend('right', legend = row.names(results), col = 1:nrow(results))
-  }
   
 })
 
