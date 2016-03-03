de4e7ec55bb3c5d2353b0130d1c4850190ff6a63
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-03 11:08:06 +0000

    define unique.zmatrix() and tax.zmatrix() as separate functions outside calculate.zmatrix()

--- R/calculate.zmatrix.R
+++ R/calculate.zmatrix.R
@@ -18,42 +18,65 @@
 #' 
 calculate.zmatrix <- function(similarity, data, lookup = NA) 
 {
-    N <- nrow(data)
-    zmatrix <- diag(N)
-    row.names <- row.names(data)
-    colnames <- row.names(data)
-    return(zmatrix)
-    
-    if(nrow(data)!=nrow(lookup)) stop('data and lookup do not match up')
-    N <- nrow(data)
-    
-    # Based on Shimatani's taxonomic similarity indices
-    species.similarity <- 1
-    genus.similarity <- 0.75
-    family.similarity <- 0.5
-    subclass.similarity <- 0.25 
-    other.similarity <- 0
-    
-    zmatrix <- diag(N)
-    for (i in 1:nrow(lookup)) {
-      for (j in 1:nrow(lookup)) {
-        
-        if (lookup$Species[i]==lookup$Species[j]) {
-          zmatrix[i,j] <- species.similarity
-        } else if (lookup$Genus[i]==lookup$Genus[j]) {
-          zmatrix[i,j] <- genus.similarity
-        } else if (lookup$Family[i]==lookup$Family[j]) {
-          zmatrix[i,j] <- family.similarity
-        } else if (lookup$Subclass[i]==lookup$Subclass[j]) {
-          zmatrix[i,j] <- subclass.similarity
-        } else zmatrix[i,j] <- other.similarity
-      }
   if(similarity=='unique') {
+    unique.zmatrix(data)
   }else if(similarity=='taxonomic') {
-    }
-    row.names(zmatrix) <- row.names(data)
-    colnames(zmatrix) <- row.names(data)
-    attr(zmatrix, 'type') <- type
-    return(zmatrix)
-  }
+    tax.zmatrix(data, lookup)
+  }else stop('Similarity not recognised')
 }
+
+
+#' 
+#' 
+#' 
+#' 
+#' 
+unique.zmatrix <- function() {
+  S <- nrow(data)
+  zmatrix <- diag(S)
+  row.names <- row.names(data)
+  colnames <- row.names(data)
+  return(zmatrix)
+}
+
+
+#' 
+#' 
+#' 
+#' 
+#' 
+tax.zmatrix <- function(data, lookup) 
+{
+  # Test
+  if(nrow(data)!=nrow(lookup)) stop('data and lookup do not match up')
+  
+  # Based on Shimatani's taxonomic similarity indices
+  species.similarity <- 1
+  genus.similarity <- 0.75
+  family.similarity <- 0.5
+  subclass.similarity <- 0.25 
+  other.similarity <- 0
+  
+  S <- nrow(data)
+  zmatrix <- diag(S)
+  for (i in 1:nrow(lookup)) {
+    for (j in 1:nrow(lookup)) {
+      
+      if (lookup$Species[i]==lookup$Species[j]) {
+        zmatrix[i,j] <- species.similarity
+      }else if(lookup$Genus[i]==lookup$Genus[j]) {
+        zmatrix[i,j] <- genus.similarity
+      }else if(lookup$Family[i]==lookup$Family[j]) {
+        zmatrix[i,j] <- family.similarity
+      }else if(lookup$Subclass[i]==lookup$Subclass[j]) {
+        zmatrix[i,j] <- subclass.similarity
+      }else zmatrix[i,j] <- other.similarity
+    }
+  }
+  row.names(zmatrix) <- row.names(data)
+  colnames(zmatrix) <- row.names(data)
+  attr(zmatrix, 'similarity') <- similarity
+  return(zmatrix)
+}
+
+
