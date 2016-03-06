99c0efaf7e46c2890b0ddb9d0ddefbba88535e9d
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-06 16:39:50 +0000

    put rdplot generic functions in the same file, for class rdiv and rdphylo

---
+++ R/rdplot.R
@@ -0,0 +1,47 @@
+
+#' rdplot()
+#' 
+#' S4 generic function \code{rdplot()}. 
+#' 
+#' @param data object of class \code{rdiv} returns a diversity profile; object 
+#' of class \linkS4class{rdphylo} returns phylogenetic tree
+#' @param ... additional parameters
+#' @return object of class \linkS4class{rdiv} returns a diversity profile; object 
+#' of class \linkS4class{rdphylo} returns phylogenetic tree
+#' 
+setGeneric(name = "rdplot", 
+           def = function(data, ...) {
+             standardGeneric("rdplot")
+           } )
+
+
+#' @describeIn rdplot plot object of class \linkS4class{rdiv}; diversity profile
+setMethod(f = "rdplot", signature = "rdiv", definition = function(data) 
+{
+  plot.this <- cbind(stack(data), row.names(data), stringsAsFactors=F)
+  colnames(plot.this) <- c('diversity', 'q', 'subcommunity')
+  plot.this$q <- as.numeric(gsub('q', '', plot.this$q))
+  
+  g <- ggplot2::ggplot() + ggplot2::theme_classic() 
+  g <- g + ggplot2::geom_line(ggplot2::aes_string(x = 'q', 
+                                                  y = 'diversity', 
+                                                  group = 'subcommunity',
+                                                  colour = 'subcommunity'), data = plot.this) 
+  g <- g + ggplot2::labs(x = bquote(italic('q')), y = data@tag)
+  
+  if(style=='big') {
+    g <- g + ggplot2::theme(text = ggplot2::element_text(size=20))
+  }
+  return(g)
+} )
+
+
+#' @describeIn rdplot plot object of class \linkS4class{rdphylo}; phylogenetic tree
+setMethod(f = "rdplot", signature = "rdphylo", 
+          definition = function(data){
+            ape::plot.phylo(data) 
+          } )
+
+
+
+
