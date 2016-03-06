b124a1d91d0da0b0b12ebde51e5f1cc9a0d45097
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-06 22:49:47 +0000

    assign is.rooted() to ape / tidy code / documentation

--- R/class-phylo.R
+++ R/class-phylo.R
@@ -1,32 +1,38 @@
-
-rdphylo <- setClass("rdphylo",
-                    contains = 'phylo',
-                    slots = c(hs.name = "vector",
-                              hs.pds = "vector",
-                              hs.edge = "matrix",
-                              hs.abundance = "vector")
-)
+#' Class 'rdphylo'
+#'
+#' @slot hs.name \code{vector} of length hS; historic species names
+#' @slot hs.pds \code{vector} of length hS; descendant present day species
+#' @slot hs.edge \code{matrix}; ancestral and descendant nodes
+#' @slot hs.abundance \code{vector} of length hS; historic species abundance
+#' @export
+#' 
+setClass("rdphylo",
+         contains = "phylo",
+         slots = c(hs.name = "vector",
+                   hs.pds = "vector",
+                   hs.edge = "matrix",
+                   hs.abundance = "vector"))
 
 
 is.rdphylo <-
-  function (x) 
+  function (x)
   {
     inherits(x, "rdphylo")
   }
 
 
-setMethod(f = "show", signature(object = "rdphylo"), 
+setMethod(f = "show", signature(object = "rdphylo"),
           definition = function(object){
-            cat('Phylogenetic tree with', length(object$tip.label), 
-                'tips and', object$Nnode, 
+            cat('Phylogenetic tree with', length(object$tip.label),
+                'tips and', object$Nnode,
                 'internal nodes (including the root.\n\n')
-            
+
             cat('Tip labels:\n', head(object$tip.label), '\n\n')
-            
-            if(is.rooted(object)) {
+
+            if(ape::is.rooted(object)) {
               rooted <- 'Rooted'
             } else rooted <- 'Unrooted'
-            
+
             cat(rooted, '.')
           } )
 
