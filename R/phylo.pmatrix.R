be03966c3f3448dfbad7c4c3d781f9799e855c41
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-06 22:45:55 +0000

    change argument 'subcommunities' to 'lookup' and tidy code

--- R/phylo.pmatrix.R
+++ R/phylo.pmatrix.R
@@ -1,25 +1,25 @@
 #' Phylogenetic population matrix
-#' 
+#'
 #' Proportional abundance of ancestral specices; PI(i,b) = (L(b)/Tbar)*pi
-#' 
+#'
 #' @param tree object of class \code{phylo}
-#' @param subcommunities \eqn{hS x 2} matrix; colnames = c('edge', 'subcommunity)
+#' @param lookup \eqn{hS x 2} matrix; colnames = c('edge', 'subcommunity)
 #' @return \eqn{hS x hS} matrix; proportional abundance of historic species
-#' 
-phylo.pmatrix <- function(tree, subcommunities) {
-  
+#'
+phylo.pmatrix <- function(tree, lookup) {
+
   new.tree <- phylo.dat(tree)
-  
+
   # Calculate proportional abundance of hs
   hs.abundance <- sapply(1:Ntype, function(x) {
     tree$edge
     tree$edge.length
-    
+
     # Define pmatrix (type = historic species)
     zmatrix <- matrix(NA, nrow = length(subcommunities), ncol = Ntype)
     colnames(zmatrix) <- subcommunities; row.names(zmatrix) <- hs
-    
-    
+
+
     # Calculate p-matrix
     pmatrix <- cbind.data.frame(human=rep(0,S),animal=rep(0,S))
     row.names(pmatrix) <- data$historic.species
@@ -28,7 +28,7 @@ phylo.pmatrix <- function(tree, subcommunities) {
     animal.index <- grep("^V",data$species)
     pmatrix$animal[animal.index] <- data$branch.abundance[animal.index]
   })
-  
+
 }
 
 
