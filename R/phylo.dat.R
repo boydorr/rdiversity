e789190f0dca17fe61014dbaa96a9e0a7e65fc7e
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-18 15:06:21 +0000

    running commentary

--- R/phylo.dat.R
+++ R/phylo.dat.R
@@ -13,24 +13,26 @@ phylo.dat <- function(tree,
                       pds.abundance = rep(1/length(tree$tip.label),
                                           length(tree$tip.label))) {
   pds.nodes <- 1:length(tree$tip.label)
-
+  
   hs.name <- unlist(sapply(pds.nodes, function(x) hs.names(tree, x)))
   Ntype <- length(hs.name)
-
+  cat(Ntype, 'historic species in phylogeny\n')
+  
   hs.pds <- sapply(hs.name, function(x) as.numeric(strsplit(x,",")[[1]][1]))
   hs.edge <- t(sapply(hs.name, function(x) as.numeric(strsplit(strsplit(x,",")[[1]][2],'-')[[1]])))
 
   # Calculate Lj for each pds; total length of evolutionary change
   Lj <- sapply(pds.nodes, function(x) calc.Lj(tree, x))
-
+  
   # Calculate the mean total evolutionary change over all pds
   Tbar <- sum(pds.abundance*Lj)
-
+  
   hs.length <- sapply(1:Ntype, function(x) {
+    cat('\rCalculating abundance of historic species..', round((x/Ntype)*100), "%")
     which.edge <- which(apply(tree$edge, 1, function(y) all.equal(y, hs.edge[x,]))==T)
     tree$edge[which.edge]
   })
-
+  
   hs.abundance <- (hs.length/Tbar) * pds.abundance[hs.pds]
 
   output <- new('rdphylo', tree,
