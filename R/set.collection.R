ac2f19386824975c2f24190d838bc0bfe8119b95
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-19 12:17:13 +0000

    New function 'set.collection()' collates pmatrix and zmatrix as an object of class initDiv

---
+++ R/set.collection.R
@@ -0,0 +1,39 @@
+#' Set Collection
+#' 
+#' This object contains 
+#' 
+#' @param data Object of class \code{matrix}
+#' @param similarity
+#' @param zmatrix
+#' @param lookup
+#' @return Object of class \code{initDiv}
+#' 
+#' @include calculate.zmatrix.R class-initDiv.R
+#' 
+set.collection <- function(data, similarity = NA, zmatrix = NA, lookup = NA) {
+  
+  # Check pmatrix
+  if(is.data.frame(data)) data <- as.matrix(data)
+  if(sum(data)!=1) data <- data / sum(data)
+  if(is.null(row.names(data))) row.names(data) <- paste('type', 1:nrow(data))
+  if(is.null(colnames(data))) colnames(data) <- paste('subcommunity', 1:ncol(data))
+  
+  if(is.matrix(zmatrix)) {
+    # If the zmatrix is provided, then the similarity argument is ignored
+    new('initDiv', data, zmatrix = zmatrix)
+    
+  }else if(is.na(zmatrix) & !is.na(similarity)) {
+    # If the similarity is provided, then calculate the zmatrix
+    zmatrix <- calculate.zmatrix(similarity, data, lookup)
+    new('initDiv', data, zmatrix = zmatrix)
+    
+  }else if(is.na(zmatrix) & is.na(similarity)) {
+    # If neither similarity nor zmatrix arguments are provided, assume a 
+    # naive-type case
+    zmatrix <- calculate.zmatrix(data)
+    new('initDiv', data, zmatrix = zmatrix)
+  }else {
+    stop('Something is wrong')
+  }
+}
+
