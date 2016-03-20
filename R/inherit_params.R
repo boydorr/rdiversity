7d6152c75dc45cad65270f4fe1fdc2e450cc4092
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-20 22:44:46 +0000

    update inherited parameter documentation

--- R/inherit_params.R
+++ R/inherit_params.R
@@ -3,7 +3,7 @@
 #' @aliases inherit_params
 #' @name inherit_params
 #'
-#' @param populations \eqn{n x m} \code{matrix} of population counts or proportions
+#' @param populations object of class \code{initDiv}
 #' @param qs \code{vector} of \emph{q} values
 #' @param Z \eqn{n x n} \code{matrix} of pair-wise similarity values
 #' 
