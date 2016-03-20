a193c3b292d061febaeb17616ac1cbc3e213f8a4
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-20 22:49:41 +0000

    update object name

--- R/phylo.pmatrix.R
+++ R/phylo.pmatrix.R
@@ -21,7 +21,7 @@ phylo.pmatrix <- function(tree, lookup) {
 
 
     # Calculate p-matrix
-    pmatrix <- cbind.data.frame(human=rep(0,S),animal=rep(0,S))
+    pmatrix <- cbind.data.frame(human=rep(0,N.hs),animal=rep(0,N.hs))
     row.names(pmatrix) <- data$historic.species
     human.index <- grep("^H",data$species)
     pmatrix$human[human.index] <- data$branch.abundance[human.index]
