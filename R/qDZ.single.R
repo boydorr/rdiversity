5437ff3674e8046388049c51012ae6063753c552
Richard Reeve <git@richardreeve.net>
2016-03-23 21:01:21 +0000

    Fixes for devtools::check(): - dQZ.single.R: reindent and add return value - DESCRIPTION: add license file name

--- R/qDZ.single.R
+++ R/qDZ.single.R
@@ -8,10 +8,10 @@
 #' @param q - order of diversity measurement
 #' @param Z - similarity matrix
 #' @param Zp - ordinariness of individuals / types in population
-#' @return 
+#' @return Similarity-sensitive diversity of order q
 #' 
 qDZ.single <-
-function(proportions, q,
-                       Z = diag(nrow(proportions)),
-                       Zp = Z %*% proportions)
-  1 / power.mean(values = Zp, order = q - 1, weights = proportions)
+  function(proportions, q,
+           Z = diag(nrow(proportions)),
+           Zp = Z %*% proportions)
+    1 / power.mean(values = Zp, order = q - 1, weights = proportions)
