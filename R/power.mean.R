1f41c9cfe6bd8e10d33d59902e9da6db0f271080
Richard Reeve <git@richardreeve.net>
2016-03-23 11:41:03 +0000

    power.mean.R: Check weights are non-negative too, and include in documentation.

--- R/power.mean.R
+++ R/power.mean.R
@@ -1,10 +1,10 @@
 #' Power mean
 #' 
-#' Calculates the order-th power mean of a single set of values, weighted by
-#' weights; by default, weights are equal and order is 1, so this is just the 
-#' arithmetic mean. Equal weights and a order of 0 gives the geometric mean,
-#' and an order of -1 gives the harmonic mean.
-#'
+#' Calculates the order-th power mean of a single set of non-negative values,
+#' weighted by weights; by default, weights are equal and order is 1, so this is
+#' just the arithmetic mean. Equal weights and a order of 0 gives the geometric
+#' mean, and an order of -1 gives the harmonic mean.
+#' 
 #' @param values Values for which to calculate mean
 #' @param order Order of power mean
 #' @param weights Weights of elements, normalised to 1 inside function
@@ -14,19 +14,19 @@
 power.mean <-
 function(values, order = 1, weights = rep(1, length(values)))
 {
-  # Normalise weights to sum to 1 (as per Rényi)
-  proportions <- weights / sum(weights)
-  
   # Check that the number of 'values' is equal to the number of 'weights'
   if (length(values) != length(weights))
     stop('The number of values does not equal the number of weights, please check arguments')
   
-  # Check that 'values' are non-negative
-  if (any(values < 0, na.rm=TRUE))
-      stop('Check that values (argument) are non-negative.')
+  # Check that 'values' and 'weights' are non-negative
+  if (any(values < 0, na.rm=TRUE) || any(weights < 0, na.rm=TRUE))
+      stop('Check that values and weights (arguments) are non-negative.')
   
-  # Check whether all proportions are NaN - happens when nothing in group
-  # In that case we want to propagate the NaN
+  # Normalise weights to sum to 1 (as per Rényi)
+  proportions <- weights / sum(weights)
+  
+  # Check whether all proportions are NaN - happens in normalisation when all
+  # weights are zero in group. In that case we want to propagate the NaN
   if (all(is.nan(proportions)))
     return(NaN)
   
