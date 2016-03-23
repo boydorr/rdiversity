2e5ca4162c974939ee9317f4ff44ee6b92c7a5c6
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-23 17:32:41 +0000

    inherit_params.R: Haven't yet changed argument populations to data. Broke documentation. Change back.

--- R/inherit_params.R
+++ R/inherit_params.R
@@ -3,7 +3,7 @@
 #' @aliases inherit_params
 #' @name inherit_params
 #'
-#' @param data object of class \code{collection}
+#' @param populations object of class \code{collection}
 #' @param qs \code{vector} of \emph{q} values
 #' 
 NULL
