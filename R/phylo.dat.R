473c1907a1a9046bad0049ec87a3f52a2412289e
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-03-24 20:18:39 +0000

    phylo.dat.R: Rename N.types to N.hs and hs.name to hs.names,

--- R/phylo.dat.R
+++ R/phylo.dat.R
@@ -13,22 +13,21 @@ as.rdphylo <- function(tree,
                       pds.abundance = rep(1/length(tree$tip.label),
                                           length(tree$tip.label))) {
   pds.nodes <- 1:length(tree$tip.label)
+  hs.names <- unlist(sapply(pds.nodes, function(x) label.hs(tree, x)))
   
-  hs.name <- unlist(sapply(pds.nodes, function(x) hs.names(tree, x)))
-  N.types <- length(hs.name)
   cat(N.types, 'historic species in phylogeny\n')
+  N.hs <- length(hs.names)
+  cat(N.hs, 'historic species in phylogeny\n')
+  hs.pds <- sapply(hs.names, function(x) as.numeric(strsplit(x,",")[[1]][1]))
+  hs.edge <- t(sapply(hs.names, function(x) as.numeric(strsplit(strsplit(x,",")[[1]][2],'-')[[1]])))
   
-  hs.pds <- sapply(hs.name, function(x) as.numeric(strsplit(x,",")[[1]][1]))
-  hs.edge <- t(sapply(hs.name, function(x) as.numeric(strsplit(strsplit(x,",")[[1]][2],'-')[[1]])))
-
   # Calculate Lj for each pds; total length of evolutionary change
   Lj <- sapply(pds.nodes, function(x) calc.Lj(tree, x))
   
   # Calculate the mean total evolutionary change over all pds
   Tbar <- sum(pds.abundance*Lj)
   
-  hs.length <- sapply(1:N.types, function(x) {
-    cat('\rCalculating abundance of historic species..', round((x/N.types)*100), "%")
+  hs.length <- sapply(1:N.hs, function(x) {
     which.edge <- which(apply(tree$edge, 1, function(y) all.equal(y, hs.edge[x,]))==T)
     tree$edge[which.edge]
   })
@@ -36,7 +35,7 @@ as.rdphylo <- function(tree,
   hs.abundance <- (hs.length/Tbar) * pds.abundance[hs.pds]
 
   output <- new('rdphylo', tree,
-                hs.name = hs.name,
+                hs.name = hs.names,
                 hs.pds = hs.pds,
                 hs.edge = hs.edge,
                 hs.abundance = hs.abundance)
