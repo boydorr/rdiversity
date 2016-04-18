43110a232fe76042fd6527b4de636ca69eddab7f
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-18 08:47:13 +0100

    as.collection.R: function to check zmatrix is valid.

--- R/as.collection.R
+++ R/as.collection.R
@@ -99,3 +99,16 @@ check.pmatrix <- function(data, zmatrix = NA) {
       colnames(pmatrix) <- paste('subcommunity', 1:ncol(pmatrix))
   }
 }
+
+
+check.zmatrix <- function(data, zmatrix) {
+  if(class(data) == 'rdphylo') {
+    if (length(data@hs.name) != nrow(zmatrix))
+      stop('Number of historic species in phylogeny must equal number of 
+           rows in zmatrix.')
+    
+  }else if(is.matrix(data)) {
+    if(any(zmatrix>1) | any(zmatrix<0)) 
+      stop('zmatrix elements must take a value between 0 and 1.')
+  }
+}
