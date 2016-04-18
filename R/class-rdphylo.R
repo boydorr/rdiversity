4a5aa70a4cf4d90723626a7345abf0c5fea6bc20
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-18 15:54:51 +0100

    class-rdphylo.R: Change instances of rdphylo to RDphylo and edit documentation.

--- R/class-rdphylo.R
+++ R/class-rdphylo.R
@@ -1,20 +1,45 @@
 setOldClass("phylo")
 
-#' Class 'rdphylo'
-#'
-#' @field hs.name \code{vector} of length \emph{hS}; historic species names
-#' @field hs.pds \code{vector} of length \emph{hS}; descendant present day 
-#' species
-#' @field hs.edge \code{matrix}; ancestral and descendant nodes
-#' @field hs.abundance \code{vector} of length \emph{hS}; historic species 
-#' abundance
-#' @field Lj \code{vector} of length \emph{S}; total evolutionary change of 
-#' present day species
-#' @field Tbar \code{numeric} element; mean total evolutionary change over 
-#' present day species
+#' Class 'RDphylo'
+#' 
+#' Container for various parameters associated with calculating phylogenetic 
+#' diversity. 
+#' 
+#' @field hs.edge two-column \code{matrix} of mode numeric; each row represents
+#' a historic species. Nodes and tips are symbolized with numbers. Tips are 
+#' numbered 1, 2, ..., and nodes are numbered after the tips. 
+#' For each row, the first column gives the ancestor.
+#' @field hs.name \code{vector} of more character; giving the names of the 
+#' historic species, ordered by the number in hs.edge.
+#' @field hs.pds \code{vector} of mode numeric; giving the present day 
+#' species descendant from each historic species, ordered by the number in 
+#' hs.edge.
+#' @field hs.abundance \code{vector} of mode numeric; giving the relative 
+#' abundance of historic species, ordered by the number in hs.edge.
+#' @field Lj \code{vector} of length \emph{S}; giving the total evolutionary 
+#' change of present day species, ordered by the number in edge.
+#' @field Tbar \code{numeric} element; giving the mean total evolutionary 
+#' change over present day species.
+#' @param object object of class \linkS4class{RDphylo}
+#' 
+#' @return 
+#' An object of class \code{RDphylo}. Builds on class \code{phylo} and 
+#' therefore contains the following components:
+#' \tabular{ll}{
+#' \code{edge} \tab two-column \code{matrix} of mode numeric; each row  
+#' represents an edge of the tree. Nodes and tips are symbolized with numbers.
+#' Tips are numbered 1, 2, ...,N and inner nodes are numbered N+1, N+2, ... 
+#' For each row, the first column gives the ancestor. \cr
+#' \code{tip.label} \tab \code{vector} of mode character; giving the names of the 
+#' tips ordered by the (positive) number in edge. \cr
+#' \code{edge.length} \tab (optional) \code{vector} of mode numeric; giving the 
+#' lengths of the branches given by edge. \cr
+#' \code{Nnode} \tab \code{integer}; number of internal nodes. \cr
+#' }
+#' 
 #' @export
 #' 
-setClass("rdphylo",
+RDphylo <- setClass("RDphylo",
          contains = "phylo",
          slots = c(hs.name = "character",
                    hs.pds = "numeric",
@@ -26,8 +51,8 @@ setClass("rdphylo",
                    ))
 
 
-
-setMethod(f = "show", signature(object = "rdphylo"),
+#' @describeIn RDphylo prints various parameters associated with the phylogeny
+setMethod(f = "show", signature(object = "RDphylo"),
           definition = function(object){
             cat('Phylogenetic tree with', length(object$tip.label),
                 'tips and', object$Nnode,
