93fc3e1936d4c5f60891be4f3d3b7d285702321b
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-18 08:46:48 +0100

    as.collection.R: function to check pmatrix is valid.

--- R/as.collection.R
+++ R/as.collection.R
@@ -88,3 +88,14 @@ is.collection <-
     inherits(x, "collection")
   }
 
+
+check.pmatrix <- function(data, zmatrix = NA) {
+  if(sum(pmatrix) != 1) {
+    pmatrix <- pmatrix / sum(pmatrix)
+    print('Population matrix was normalised to sum to 1.')
+    if(is.null(row.names(pmatrix))) 
+      row.names(pmatrix) <- paste('type', 1:nrow(pmatrix))
+    if(is.null(colnames(pmatrix))) 
+      colnames(pmatrix) <- paste('subcommunity', 1:ncol(pmatrix))
+  }
+}
