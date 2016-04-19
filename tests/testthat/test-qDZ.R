0b63cd2a3c2b402ec5c36e4ede0fc15a130a460f
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-19 09:24:08 +0100

    Tests: Replace 'collection' with 'supercommunity'.

--- tests/testthat/test-qDZ.R
+++ tests/testthat/test-qDZ.R
@@ -8,9 +8,9 @@ numcommunities <- 8
 manyweights <- t(gtools::rdirichlet(numcommunities, rep(1, numspecies)))
 
 test_that ("The qDZ() function works for a single population", {
-  expect_equivalent(qDZ(as.collection(weights), 1:2), 
+  expect_equivalent(qDZ(as.supercommunity(weights), 1:2), 
                     c(qD(weights, 1:2)))
-  expect_equivalent(qDZ(as.collection(weights, zmatrix = Z1), 
+  expect_equivalent(qDZ(as.supercommunity(weights, zmatrix = Z1), 
                         qs = c(0:3,Inf)), 
                     rep(1,5))
 })
@@ -18,7 +18,7 @@ test_that ("The qDZ() function works for a single population", {
 
 test_that ("The qDZ() function works for a multiple populations", {   
   Z2 <- matrix(rep(1, numspecies*numspecies), ncol=numspecies)
-expect_equivalent(qDZ(as.collection(manyweights, zmatrix = Z2), c(0:2, Inf)),
+expect_equivalent(qDZ(as.supercommunity(manyweights, zmatrix = Z2), c(0:2, Inf)),
                  data.frame(matrix(rep(1,(4 * dim(manyweights)[2])),ncol=4)))
 })
 
@@ -26,6 +26,6 @@ context('Testing qDZ() warning generation')
 pop <- c(0.1, 0.1)
 
 test_that ("Generate warnings, but normalise and calculate diversities", {
-  # expect_equivalent(qDZ(as.collection(pop/sum(pop)), 1), 2)  
-  expect_error(qDZ(as.collection(pop), 1))  
+  # expect_equivalent(qDZ(as.supercommunity(pop/sum(pop)), 1), 2)  
+  expect_error(qDZ(as.supercommunity(pop), 1))  
 })
