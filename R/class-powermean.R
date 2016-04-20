c4acc711a6cc1d71d67f72668ec6c6e70555c6d1
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-20 17:15:19 +0100

    class-powermean.R: tidy code.

--- R/class-powermean.R
+++ R/class-powermean.R
@@ -7,22 +7,15 @@
 #' @param object object of class \linkS4class{powermean}
 #' @export
 #' 
-powermean <- setClass("powermean",
-                 contains = 'matrix',
-                 slots = c(.Data = "matrix",
-                           measure = "character"))
+setClass("powermean", contains = "matrix",
+         slots = c(.Data = "matrix", 
+                   measure = "character"))
 
-
-is.powermean <-
-  function (x) 
-  {
-    inherits(x, "powermean")
-  }
 
 #' @describeIn powermean Prints power mean
 setMethod(f = "show", signature = "powermean", 
           definition = function(object) {
             cat(object@measure, '\n\n')
-            print(object) 
-          } )
+            print(object) } )
+
 
