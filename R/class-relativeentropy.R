c2cb2715bb8ebcec9688d4fa84f931946f618de7
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-20 17:15:53 +0100

    class-relativeentropy.R: tidy code.

--- R/class-relativeentropy.R
+++ R/class-relativeentropy.R
@@ -7,22 +7,13 @@
 #' @param object object of class \linkS4class{relativeentropy}
 #' @export
 #' 
-relativeentropy <- setClass("relativeentropy",
-                      contains = "matrix",
-                      slots = c(.Data = "matrix",
-                                measure = "character"))
+setClass("relativeentropy", contains = "matrix",
+         slots = c(.Data = "matrix", measure = "character"))
 
-
-is.relativeentropy <-
-  function (x) 
-  {
-    inherits(x, "relativeentropy")
-  }
 
 #' @describeIn relativeentropy Prints relative entropy
 setMethod(f = "show", signature = "relativeentropy", 
           definition = function(object) {
             cat(object@measure, '\n\n')
-            print(object) 
-          } )
+            print(object) } )
 
