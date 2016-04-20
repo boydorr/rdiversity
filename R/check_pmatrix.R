55987a52fb7677a7a7732a6c83a454a100e2cb21
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-20 17:00:14 +0100

    check_pmatrix.R: Rename file and function.

--- R/check_pmatrix.R
+++ R/check_pmatrix.R
@@ -1,8 +1,8 @@
-check.pmatrix <- function(data, zmatrix = NA) {
+check_pmatrix <- function(data, zmatrix = NA) {
   pmatrix <- data
   if(sum(pmatrix) != 1) {
     pmatrix <- pmatrix / sum(pmatrix)
-    print('Population matrix was normalised to sum to 1.')
+    warning('Population matrix was normalised to sum to 1.')
     if(is.null(row.names(pmatrix))) 
       row.names(pmatrix) <- paste('type', 1:nrow(pmatrix))
     if(is.null(colnames(pmatrix))) 
