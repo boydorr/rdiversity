9d6422d54d2f0ff93f622e12d92bc6acc429e290
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-21 19:20:30 +0100

    check_partition.R: add comments

--- R/check_partition.R
+++ R/check_partition.R
@@ -1,11 +1,9 @@
 #' Check partition
 #' 
-#' sdf
+#' \code{check_partition()} is used to validate partition matrices.
 #' 
 #' @param partition two-dimensional \code{matrix} of mode \code{numeric}; 
 #' contains relative abundance of types
-#' 
-#' @details 
 #' 
 #' @return Returns a two-dimensions \code{matrix} of mode \code{numeric}. If 
 #' the partition matrix was valid, this should be identical to that which was
@@ -13,16 +11,23 @@
 #' @export
 #' 
 #' @examples 
+#' population <- data.frame(a = 1:2, b = 3:4)
+#' row.names(population) <- NULL
+#' new.population <- check_partition(population)
+#' new.population
 #' 
 check_partition <- function(partition) {
   if(is.data.frame(partition)) partition <- as.matrix(partition)
+  # normalise partition if it does not sum to 1
   if(sum(partition) != 1) {
     partition <- partition / sum(partition)
     warning('Population matrix was normalised to sum to 1.')
-    if(is.null(row.names(partition))) 
-      row.names(partition) <- paste('type', 1:nrow(partition))
-    if(is.null(colnames(partition))) 
-      colnames(partition) <- paste('subcommunity', 1:ncol(partition))
   }
+  # add labels if rows and columns have none
+  if(is.null(row.names(partition))) 
+    row.names(partition) <- paste('type', 1:nrow(partition))
+  if(is.null(colnames(partition))) 
+    colnames(partition) <- paste('subcommunity', 1:ncol(partition))
+  
   return(partition)
 }
