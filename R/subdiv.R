7700121b3a0ed0602f86d93b742c687ce314e1f5
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-21 14:48:59 +0100

    subdiv.R: Take type_weights from supercommunity object

--- R/subdiv.R
+++ R/subdiv.R
@@ -3,13 +3,11 @@
 #' 
 #' 
 subdiv <- function(data, qs) {
-  # weight <- sapply(1:ncol(data), function(x)
-  #   (data@type_abundance[,x]/colSums(data@type_abundance)[x]))
   
   results <- list()
   for(i in 1:length(qs))
   results[[i]] <- sapply(1:ncol(data), function(y) 
-    power.mean(data[,y], i, weight[,y]))
+    power.mean(data[,y], i, data@type_weights[,y]))
   
   results <- data.frame(matrix(unlist(results), 
                                nrow = length(results), 
