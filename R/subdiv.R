00778c4ed337a0725fc48ecb6025122217b1d03d
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-21 17:07:13 +0100

    subdiv.R: Fix qs argument.

--- R/subdiv.R
+++ R/subdiv.R
@@ -12,11 +12,10 @@
 #' @examples as
 #' 
 subdiv <- function(data, qs) {
-  
   results <- list()
   for(i in 1:length(qs))
   results[[i]] <- sapply(1:ncol(data), function(y) 
-    power.mean(data[,y], i, data@type_weights[,y]))
+    power.mean(data[,y], 1-qs[i], data@type_weights[,y]))
   
   results <- data.frame(matrix(unlist(results), 
                                nrow = length(results), 
