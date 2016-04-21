0726d06fa0eb8cd97e5f97b8e8c1b8f4cc988653
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-21 19:34:43 +0100

    as.relativeentropy.R:  Create functions for relativeentropy(), as.relativeentropy() and is.relativeentropy()

---
+++ R/as.relativeentropy.R
@@ -0,0 +1,41 @@
+#' Coerse to relativeentropy
+#' 
+#' Functions to check if an object is a \code{relativeentropy} or coerce an  
+#' object into a \code{relativeentropy}.
+#' 
+#' @param results two-dimensional \code{matrix} of mode \code{numeric}; 
+#' @param super object of class \code{supercommunity}; 
+#' @param tag object of class \code{character}; 
+#' 
+#' @details 
+#' 
+#' @return object of class \code{relativeentropy}
+#' @include class-relativeentropy.R 
+#' @export
+#' 
+#' @examples 
+#' 
+relativeentropy <- function(results, super, tag) {
+  new('relativeentropy', 
+      results, 
+      measure = tag,
+      type_abundance = super@type_abundance,
+      ordinariness = super@ordinariness,
+      subcommunity_weights = super@subcommunity_weights,
+      type_weights = super@type_weights)
+}
+
+
+#' @rdname relativeentropy
+#'
+as.relativeentropy <- relativeentropy
+
+
+#' @rdname relativeentropy
+#' @param x any R object 
+#' @return \code{is.relativeentropy(x)} returns TRUE if its argument is a 
+#' relativeentropy, FALSE otherwise.
+#' @export 
+#' 
+is.relativeentropy <- function (x) 
+  inherits(x, "relativeentropy")
