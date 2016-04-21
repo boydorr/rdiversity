439dd7e63f46804f825d6d8bf719313a8ee2d490
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-21 19:21:06 +0100

    class-relativeentropy.R: add more slots, edit show() and aadd documentation.

--- R/class-relativeentropy.R
+++ R/class-relativeentropy.R
@@ -1,14 +1,32 @@
-#' Class 'relativeentropy'
+#' relativeentropy-class
 #' 
 #' Container for 
 #' 
-#' @field .Data 
-#' @field measure 
+#' @field .Data two-dimensional \code{matrix} of mode \code{numeric}; contains 
+#' diversity term values 
+#' @field measure object of class \code{character}; contains an identifier 
+#' associated with the diversity term being calculated
+#' @field type_abundance two-dimensional \code{matrix} of mode \code{numeric}; 
+#' contains proportional abundance of \emph{types} in the subcommunity 
+#' as a fraction of the supercommunity as a whole (in the phylogenetic case, 
+#' this corresponds to the proportional abundance of historic species, which
+#' is calculated from the proportional abundance of present day species)
+#' @field ordinariness two-dimensional \code{matrix} of mode \code{numeric}; 
+#' contains ordinariness of types 
+#' @field subcommunity_weights \code{vector} of mode \code{numeric}; contains
+#' subcommunity weights
+#' @field type_weights two-dimensional \code{matrix} of mode \code{numeric}; 
+#' contains weight of types within a subcommunity
 #' 
 #' @export
 #' 
 setClass("relativeentropy", contains = "matrix",
-         slots = c(.Data = "matrix", measure = "character"))
+         slots = c(.Data = "matrix", 
+                   measure = "character",
+                   type_abundance = "matrix",
+                   ordinariness = "matrix",
+                   subcommunity_weights = "vector",
+                   type_weights = "matrix"))
 
 
 #' @describeIn relativeentropy Prints relative entropy
@@ -16,6 +34,6 @@ setClass("relativeentropy", contains = "matrix",
 #' 
 setMethod(f = "show", signature = "relativeentropy", 
           definition = function(object) {
-            cat(object@measure, '\n\n')
-            print(object) } )
+            print(object@.Data)
+          } )
 
