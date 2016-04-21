775e26b8212f27f9aca054905b889a29ffbf9cc1
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-21 19:33:01 +0100

    superdiv.R: Convert function to generic and method specific for powermean

--- R/superdiv.R
+++ R/superdiv.R
@@ -12,20 +12,29 @@
 #' 
 #' @examples 
 #' 
-superdiv <- function(data, qs) {
-  results <- list()
-  for(i in 1:length(qs)) {
-    subdiv <- sapply(1:ncol(data), function(y) 
-      power.mean(data[,y], 1-qs[i], data@type_weights[,y]))
-    results[[i]] <- power.mean(subdiv, 1-qs[i], data@subcommunity_weights)
-  }
-   
-  results <- data.frame(matrix(unlist(results), 
-                               nrow = length(results), 
-                               byrow=T), 
-                        stringsAsFactors=FALSE)
-  colnames(results) <- paste("supercommunity", data@measure)
-  row.names(results) <- paste0("q", qs)
-  
-  return(results)
-}
+setGeneric(name = "superdiv",
+           def = function(data, qs) {
+             standardGeneric("superdiv")
+           } )
+
+
+#' @rdname superdiv
+#' 
+setMethod(f = "superdiv", signature= "powermean", 
+          definition = function(data, qs) {  
+            results <- list()
+            for(i in 1:length(qs)) {
+              subdiv <- sapply(1:ncol(data), function(y) 
+                power.mean(data[,y], 1-qs[i], data@type_weights[,y]))
+              results[[i]] <- power.mean(subdiv, 1-qs[i], data@subcommunity_weights)
+            }
+            
+            results <- data.frame(matrix(unlist(results), 
+                                         nrow = length(results), 
+                                         byrow=T), 
+                                  stringsAsFactors=FALSE)
+            colnames(results) <- paste("supercommunity", data@measure)
+            row.names(results) <- paste0("q", qs)
+            
+            return(results)
+          } )
