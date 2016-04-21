6260e3cca66fe44f26051dbc9472f64ed1afc06b
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-21 19:33:18 +0100

    superdiv.R: Create method for relativeentropy.

--- R/superdiv.R
+++ R/superdiv.R
@@ -38,3 +38,27 @@ setMethod(f = "superdiv", signature= "powermean",
             
             return(results)
           } )
+
+
+#' @rdname superdiv
+#' 
+setMethod(f = "superdiv", signature= "relativeentropy", 
+          definition = function(data, qs) {  
+            results <- list()
+            for(i in 1:length(qs)) {
+              subdiv <- sapply(1:ncol(data), function(y) 
+                power.mean(data[,y], qs[i]-1, data@type_weights[,y]))
+              results[[i]] <- power.mean(subdiv, 1-qs[i], data@subcommunity_weights)
+            }
+            
+            results <- data.frame(matrix(unlist(results), 
+                                         nrow = length(results), 
+                                         byrow=T), 
+                                  stringsAsFactors=FALSE)
+            colnames(results) <- paste("supercommunity", data@measure)
+            row.names(results) <- paste0("q", qs)
+            
+            return(results)
+          } )
+          
+          
