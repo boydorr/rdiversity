bb500b5dd47e6fec602163338461950c456551db
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-21 19:23:53 +0100

    subdiv.R: convert function to a powermean class specific method.

--- R/subdiv.R
+++ R/subdiv.R
@@ -14,18 +14,25 @@
 #' @export 
 #' @examples as
 #' 
-subdiv <- function(data, qs) {
-  results <- list()
-  for(i in 1:length(qs))
-  results[[i]] <- sapply(1:ncol(data), function(y) 
-    power.mean(data[,y], 1-qs[i], data@type_weights[,y]))
-  
-  results <- data.frame(matrix(unlist(results), 
-                               nrow = length(results), 
-                               byrow=T), 
-                        stringsAsFactors=FALSE)
-  colnames(results) <- colnames(data)
-  row.names(results) <- paste0("q", qs)
-  
-  return(results)
-}
+setGeneric(name = "subdiv",
+           def = function(data, qs) {
+             standardGeneric("subdiv")
+           } )
+
+#' @rdname subdiv
+#' 
+setMethod(f = "subdiv", signature= "powermean", 
+          definition = function(data, qs) {
+            results <- list()
+            for(i in 1:length(qs))
+              results[[i]] <- sapply(1:ncol(data), function(y) 
+                power.mean(data[,y], 1-qs[i], data@type_weights[,y]))
+            
+            results <- data.frame(matrix(unlist(results), 
+                                         nrow = length(results), 
+                                         byrow=T), 
+                                  stringsAsFactors=FALSE)
+            colnames(results) <- colnames(data)
+            row.names(results) <- paste0("q", qs) 
+            return(results)
+            } )
