275eb7cfff3a4691f8682a79306b4802c116a641
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-26 11:34:57 +0100

    test-qDZ.R: renamed arguments

--- tests/testthat/test-qDZ.R
+++ tests/testthat/test-qDZ.R
@@ -10,15 +10,14 @@ manyweights <- t(gtools::rdirichlet(numcommunities, rep(1, numspecies)))
 test_that ("The qDZ() function works for a single population", {
   expect_equivalent(qDZ(as.supercommunity(weights), 1:2), 
                     c(qD(weights, 1:2)))
-  expect_equivalent(qDZ(as.supercommunity(weights, zmatrix = Z1), 
-                        qs = c(0:3,Inf)), 
+  expect_equivalent(qDZ(as.supercommunity(weights, Z1), qs = c(0:3,Inf)), 
                     rep(1,5))
 })
 
 
 test_that ("The qDZ() function works for a multiple populations", {   
   Z2 <- matrix(rep(1, numspecies*numspecies), ncol=numspecies)
-expect_equivalent(qDZ(as.supercommunity(manyweights, zmatrix = Z2), c(0:2, Inf)),
+expect_equivalent(qDZ(as.supercommunity(manyweights, Z2), c(0:2, Inf)),
                  data.frame(matrix(rep(1,(4 * dim(manyweights)[2])),ncol=4)))
 })
 
