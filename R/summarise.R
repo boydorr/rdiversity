0eb847d14177a312b5012f6143ca7bc534477740
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-27 11:20:07 +0100

    summarise.R: reindent lines.

--- R/summarise.R
+++ R/summarise.R
@@ -15,23 +15,23 @@
 #' @examples 
 #' 
 summarise <-
-{
-  totals <- array(rowSums(populations), dim=c(dim(populations)[1], 1))
-  
-  if (normalise)
   function(populations, normalise = TRUE)
   {
-    total <- sum(totals)
-    totals <- totals / total
-    proportions <- populations / total
-    weights <- colSums(proportions)
-    proportions <- proportions %*% diag(1/(weights))
-  } else {
-    proportions <- populations
-    weights <- colSums(proportions)
+    totals <- array(rowSums(populations), dim=c(dim(populations)[1], 1))
+    
+    if (normalise)
+    {
+      total <- sum(totals)
+      totals <- totals / total
+      proportions <- populations / total
+      weights <- colSums(proportions)
+      proportions <- proportions %*% diag(1/(weights))
+    } else {
+      proportions <- populations
+      weights <- colSums(proportions)
+    }
+    num <- length(weights)
+    
+    list(proportions=proportions, totals=totals,
+         weights=array(weights, dim=c(1, num)), num=num)
   }
-  num <- length(weights)
-  
-  list(proportions=proportions, totals=totals,
-       weights=array(weights, dim=c(1, num)), num=num)
-}
