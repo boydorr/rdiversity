af298cc8abbd057cff11e7650bc04aac144d9509
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-28 17:12:39 +0100

    as.supercommunity.R: make print() work in a useful way.

--- R/as.supercommunity.R
+++ R/as.supercommunity.R
@@ -223,5 +223,20 @@ is.supercommunity <- function (x)
 #' 
 setMethod(f = "show", signature= "supercommunity", 
           definition = function(object) {
-            print(head(object@.Data)) } )
+            cat('Type abundance:', 
+                paste0('[', nrow(object@type_abundance), 'x', 
+                       ncol(object@type_abundance), ']'), '\n\n')
+            print(head(object@type_abundance),10) 
+            if(nrow(object@type_abundance)>10)
+              cat("..\t","...\n\n\n")
+            
+            cat('Type ordinariness:',
+                paste0('[', nrow(object@ordinariness), 'x', 
+                       ncol(object@ordinariness), ']'), '\n\n')
+            print(head(object@ordinariness),10) 
+            if(nrow(object@ordinariness)>10)
+              cat("..\t","...\n\n\n")
+            } )
+
+
 
