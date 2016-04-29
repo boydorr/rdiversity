cb3007ce7fd0adac665ea8be791a8a82f1954246
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-04-29 17:36:54 +0100

    qDZ.single.R: check partition.

--- R/qDZ.single.R
+++ R/qDZ.single.R
@@ -28,5 +28,7 @@
 qDZ.single <-
   function(proportions, q,
            Z = diag(nrow(proportions)),
-           Zp = Z %*% proportions)
+           Zp = Z %*% proportions) {
+    proportions <- check_partition(proportions)
     1 / power.mean(values = Zp, order = q - 1, weights = proportions)
+  }
