e503803be2e92cef150e49fc6f560e164c14cb4d
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-05-03 20:31:20 +0100

    as.supercommunity.R: edit print() format

--- R/as.supercommunity.R
+++ R/as.supercommunity.R
@@ -248,14 +248,18 @@ setMethod(f = "show", signature= "supercommunity",
           definition = function(object) {
             cat('Type abundance:', 
                 paste0('[', nrow(object@type_abundance), 'x', 
-                       ncol(object@type_abundance), ']'), '\n\n')
+                       ncol(object@type_abundance), 
+                       ']\n------------------\n'))
             print(head(object@type_abundance),10) 
-            if(nrow(object@type_abundance)>10)
-              cat("..\t","...\n\n\n")
+            if(nrow(object@type_abundance)>10) {
+              cat("..\t","...\n\n")
+            } else
+              cat("\n\n")
             
             cat('Type ordinariness:',
                 paste0('[', nrow(object@ordinariness), 'x', 
-                       ncol(object@ordinariness), ']'), '\n\n')
+                       ncol(object@ordinariness), 
+                       ']\n------------------\n'))
             print(head(object@ordinariness),10) 
             if(nrow(object@ordinariness)>10)
               cat("..\t","...\n\n\n")
