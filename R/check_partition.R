7d40df7fc1a86a4b60fd12d944376bd73da675ca
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-05-06 14:06:08 +0100

    check_partition.R: make this a warning.

--- R/check_partition.R
+++ R/check_partition.R
@@ -23,7 +23,7 @@ check_partition <- function(partition) {
   # normalise partition if it does not sum to 1
   if(!isTRUE(all.equal(sum(partition),1))) {
     partition <- partition / sum(partition)
-    print('Population matrix was normalised to sum to 1.')
+    warning ('Population matrix was normalised to sum to 1.')
   }
   
   # add labels if rows and columns have none
