a45c4814ab970fc627f145d83900d1c86bcb72a8
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-05-09 21:06:42 +0100

    test-phylo.R: parameters are now stored in data_frame.

--- tests/testthat/test-phylo.R
+++ tests/testthat/test-phylo.R
@@ -5,11 +5,11 @@ test_that("Answers match up with Leinster-Cobbold Appendix A", {
   # Ultrametric
   phy <- ape::read.tree(text="(A:2,B:2)R:1;")
   pds <- c(0.6, 0.4)
-  x <- as.rdphylo(phy,pds)
+  x <- as.rdphylo(phy, pds)
   super <- supercommunity(phy, pds.abundance = pds)
   
   expect_equivalent(sum(phy$edge.length) + phy$root.edge, 
-                    c(superdiv(gamma(super),0) * x@Tbar))
+                    c(superdiv(gamma(super),0) * x@parameters$Tbar[1]))
   expect_equivalent(c(super@type_abundance), 
                     c(0.4, (2/3)*0.4, 0.2, (1/3)*0.4))
   expect_equivalent(super@similarity, 
@@ -22,7 +22,7 @@ test_that("Answers match up with Leinster-Cobbold Appendix A", {
   super2 <- supercommunity(phy2, pds.abundance = pds)
   
   expect_equivalent(sum(phy2$edge.length) + phy2$root.edge, 
-                    c(superdiv(gamma(super2),0) * x2@Tbar))
+                    c(superdiv(gamma(super2),0) * x2@parameters$Tbar[1]))
   expect_equivalent(c(super2@type_abundance), 
                     c(0.25, (2/2.4)*0.4, 0.25, (1/2.4)*0.4))
   expect_equivalent(super2@similarity, 
