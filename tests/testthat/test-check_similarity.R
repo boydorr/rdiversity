fea6bac04d2294a63fea724841b0a41d2be9b549
Richard Reeve <git@richardreeve.net>
2016-06-14 15:30:02 +0100

    Typo in error test.

--- tests/testthat/test-check_similarity.R
+++ tests/testthat/test-check_similarity.R
@@ -3,7 +3,7 @@ context('Testing check_similarity()')
 test_that("The function check_similarity() gives the correct errors", {
   partition <- matrix(rep(1, 9), nrow = 3)
     
-  expect_error(check_similarity(partiton, matrix(rep(-1,9), nrow=3)))
+  expect_error(check_similarity(partition, matrix(rep(-1,9), nrow=3)))
   # expect_error(check_similarity(partition, diag(2,3)))
   expect_error(check_similarity(partition, matrix(rep(1,6), nrow=3)))
   expect_error(check_similarity(partition, matrix(rep(1,4), nrow=2)))
