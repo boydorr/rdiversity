a3aed78e027b45905049523dd45e69e90325c9e5
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-10-20 12:46:32 +0100

    deprecate functions: superdiv(), supercommunity(), normalised.supercommunity.rho(), and so on. To align with changes from supercommunity to metacommunity.

--- R/rdiversity-package.R
+++ R/rdiversity-package.R
@@ -28,3 +28,61 @@
 #' 
 #' 
 NULL
+
+
+# In a later release, change .Deprecated to .Defunct.
+
+raw.supercommunity.alpha <- function(super, qs) {
+  .Deprecated("raw.metacommunity.alpha") 
+  raw.metacommunity.alpha(super, qs)
+}  
+
+normalised.supercommunity.alpha <- function(super, qs) {
+  .Deprecated("normalised.metacommunity.alpha") 
+  normalised.metacommunity.alpha(super, qs)
+}  
+
+raw.supercommunity.beta <- function(super, qs) {
+  .Deprecated("raw.metacommunity.beta") 
+  raw.metacommunity.beta(super, qs)
+}  
+
+normalised.supercommunity.beta <- function(super, qs) {
+  .Deprecated("normalised.metacommunity.beta") 
+  normalised.metacommunity.beta(super, qs)
+}
+
+raw.supercommunity.rho <- function(super, qs) {
+  .Deprecated("raw.metacommunity.rho") 
+  raw.metacommunity.rho(super, qs)
+}
+
+normalised.supercommunity.rho <- function(super, qs) {
+  .Deprecated("normalised.metacommunity.rho") 
+  normalised.metacommunity.rho(super, qs)
+}
+
+supercommunity.gamma <- function(super, qs) {
+  .Deprecated("metacommunity.gamma") 
+  metacommunity.gamma(super, qs)
+}
+
+is.supercommunity <- function(x) {
+  .Deprecated("is.metacommunity") 
+  is.metacommunity(x)
+}
+
+as.supercommunity <- function(partition, similarity) {
+  .Deprecated("as.metacommunity") 
+  as.metacommunity(partition, similarity)
+}
+
+supercommunity <- function(partition, similarity) {
+  .Deprecated("metacommunity") 
+  metacommunity(partition, similarity)
+}
+
+superdiv <- function(data, qs) {
+  .Deprecated("metadiv") 
+  metadiv(data, qs)
+}
