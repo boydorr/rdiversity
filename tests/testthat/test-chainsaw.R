cf365b28cb5ae0f25b80b23fac0db50573f8a6c2
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2016-10-20 12:42:23 +0100

    all tests: change all instances of super, supercommunities, etc. to meta, metacommunities, and so on.

--- tests/testthat/test-chainsaw.R
+++ tests/testthat/test-chainsaw.R
@@ -5,13 +5,13 @@ test_that("Answers match up in the ultrametric case", {
   leaf.abundance <- c(0.6, 0.4)
   data <- as.rdphylo(leaf.abundance, tree)
   interval <- 2
-  super <- chainsaw(data, leaf.abundance, interval)
+  meta <- chainsaw(data, leaf.abundance, interval)
   
   tree2 <- ape::read.tree(text="(A:2,B:2)R:2;")
-  super2 <- metacommunity(leaf.abundance, tree2) # type_weights
+  meta2 <- metacommunity(leaf.abundance, tree2) # type_weights
   
-  expect_equivalent(normalised.metacommunity.alpha(super, 0:2), 
-                    normalised.metacommunity.alpha(super2, 0:2))
+  expect_equivalent(normalised.metacommunity.alpha(meta, 0:2), 
+                    normalised.metacommunity.alpha(meta2, 0:2))
   
 })
   
@@ -21,20 +21,20 @@ test_that("Answers match up in the non-ultrametric case", {
   leaf.abundance <- c(0.6, 0.4)
   data <- as.rdphylo(leaf.abundance, tree)
   interval <- 2
-  expect_warning(super <- chainsaw(data, leaf.abundance, interval))
+  expect_warning(meta <- chainsaw(data, leaf.abundance, interval))
   
   tree2 <- ape::read.tree(text="(A:1,B:2)R:2;")
-  super2 <- metacommunity(leaf.abundance, tree2) # type_weights
+  meta2 <- metacommunity(leaf.abundance, tree2) # type_weights
   
-  expect_equivalent(normalised.metacommunity.alpha(super, 0:2), 
-                    normalised.metacommunity.alpha(super2, 0:2))
+  expect_equivalent(normalised.metacommunity.alpha(meta, 0:2), 
+                    normalised.metacommunity.alpha(meta2, 0:2))
 })
 
 
 test_that("Setting chainsaw interval to 1 returns the phylogeny intact", {
   tree2 <- ape::read.tree(text="(A:1,B:2)R:2;")
   leaf.abundance <- c(0.6, 0.4)
-  super2 <- metacommunity(leaf.abundance, tree2) # type_weights
+  meta2 <- metacommunity(leaf.abundance, tree2) # type_weights
   
-expect_equal(super2, chainsaw(rdphylo(leaf.abundance, tree2), leaf.abundance, 1))
+expect_equal(meta2, chainsaw(rdphylo(leaf.abundance, tree2), leaf.abundance, 1))
 })
--- tests/testthat/test-phylo.R
+++ tests/testthat/test-phylo.R
@@ -5,26 +5,26 @@ test_that("Answers match up with Leinster-Cobbold Appendix A", {
   phy <- ape::read.tree(text="(A:2,B:2)R:1;")
   pds <- c(0.6, 0.4)
   x <- as.rdphylo(pds, phy)
-  super <- metacommunity(pds, phy)
+  meta <- metacommunity(pds, phy)
   
   expect_equivalent(sum(phy$edge.length) + phy$root.edge, 
-                    unlist(metadiv(raw.gamma(super),0)$diversity * x@Tbar))
-  expect_equivalent(c(super@type_abundance), 
+                    unlist(metadiv(raw.gamma(meta),0)$diversity * x@Tbar))
+  expect_equivalent(c(meta@type_abundance), 
                     c(0.2, 0.4, (1/3)*0.4, (2/3)*0.4))
-  expect_equivalent(super@similarity, 
+  expect_equivalent(meta@similarity, 
                     rbind(rep(1,4),c(1,1,0,0),rep(1,4),c(0,0,1,1)))
   
   # Non-ultrametric
   phy2 <- ape::read.tree(text="(A:1,B:2)R:1;")
   pds <- c(0.6, 0.4)
   x2 <- as.rdphylo(pds, phy2)
-  super2 <- metacommunity(pds, phy2)
+  meta2 <- metacommunity(pds, phy2)
   
   expect_equivalent(sum(phy2$edge.length) + phy2$root.edge, 
-                    unlist(metadiv(raw.gamma(super2),0)$diversity * x2@Tbar[1]))
-  expect_equivalent(c(super2@type_abundance), 
+                    unlist(metadiv(raw.gamma(meta2),0)$diversity * x2@Tbar[1]))
+  expect_equivalent(c(meta2@type_abundance), 
                     c(0.25, 0.25, (1/2.4)*0.4, (2/2.4)*0.4))
-  expect_equivalent(super2@similarity, 
+  expect_equivalent(meta2@similarity, 
                     rbind(c(1.2, 1.2, 0.8, 0.8), c(1.2, 1.2, 0, 0),
                           c(1.2, 1.2, 0.8, 0.8), c(0, 0, 0.8, 0.8)))
 })
