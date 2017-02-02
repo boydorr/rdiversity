0f5f11122af2a9c5f4ff31e33d0c8d48f9d63881
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-02-02 11:20:05 +0000

    as.metacommunity.R: Nevermind the last commit. Delete this line to avoid future confusion.

--- R/as.metacommunity.R
+++ R/as.metacommunity.R
@@ -294,7 +294,6 @@ setMethod(f = "metacommunity",
             type_abundance <- do.call(rbind, type_abundance)
             row.names(type_abundance) <- hs.abundance$hs.name
             colnames(type_abundance) <- colnames(partition)
-            type_abundance <- check_partition(type_abundance)
 
             subcommunity_weights <- colSums(type_abundance) /
               sum(type_abundance)
