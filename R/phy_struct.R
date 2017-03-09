f30909f0882d99022dae9a3c5da08166f34b3fcd
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-03-09 14:18:08 +0000

    phy_struct(): change slot name for consistency.

--- R/phy_struct.R
+++ R/phy_struct.R
@@ -110,7 +110,7 @@ setMethod(f = "show", signature(object = "phy_struct"),
           definition = function(object){
             all_tips <- sum(colSums(object@structure) > 0)
             Nhs <- sum(rowSums(object@structure) > 0)
-            cat('@structure: Structural matrix describing', all_tips, 'tips and',
+            cat('@phylo_struct: Structural matrix describing', all_tips, 'tips and',
                 Nhs, 'historic species.\n')
             cat('@parameters: Historic species parameters.')
           })
