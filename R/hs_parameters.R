747d697b252d0c50eb97a019cb3b785d19d2296c
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-03-10 10:16:40 +0000

    Extract hs_parameters() from phy_struct()

---
+++ R/hs_parameters.R
@@ -0,0 +1,54 @@
+#' hs_parameters
+#'
+#' hs_parameters
+#' 
+#' @param tree object of class \code{phylo}
+#' 
+#' @return Returns parameters associated with each historic species 
+#' @export
+#' 
+hs_parameters <- function(tree) {
+  # Perform checks
+  if(class(tree) != "phylo") stop("'tree' argument must be class phylo.")
+  
+  # If root has a length
+  root_ancestor = 0
+  long_root <- ifelse(!is.null(tree$root.edge), TRUE, FALSE)
+  
+  # Ancestral species
+  ancestral_nodes <- lapply(as.list(seq_along(tree$tip.label)), function(x) {
+    res <- c(x, phangorn::Ancestors(tree, x, 'all'))
+    if(long_root) c(res, root_ancestor) else res
+  })
+  
+  root_node <- ape::Ntip(tree) + 1
+  
+  if(long_root) {
+    tree$edge <- rbind(tree$edge, c(0, root_node))
+    tree$edge.length <- c(tree$edge.length, tree$root.edge)
+  }
+  
+  # Historic species data for each present day species
+  parameters <- lapply(seq_along(ancestral_nodes), function(x) {
+    daughters <- ancestral_nodes[[x]]
+    if(long_root) daughters <- c(daughters, 0)
+    daughters <- daughters[-length(daughters)]
+    res <- lapply(as.list(daughters), function(y) tree$edge[tree$edge[,2] %in% y,])
+    res <- do.call(rbind.data.frame, res)
+    res <- cbind.data.frame(tree$tip.label[x], x, res)
+    colnames(res) <- c("tip_label", "tip_node", "a_node", "d_node")
+    lengths <- sapply(res$d_node, function(x) which(tree$edge[,2] %in% x))
+    lengths <- tree$edge.length[lengths]
+    cbind.data.frame(res, lengths)
+  })
+  parameters <- do.call(rbind.data.frame, parameters)
+  
+  # Historic species names
+  hs_names <- paste(parameters$tip_node, 
+                    paste(parameters$a_node, 
+                          parameters$d_node, sep = "-"), sep = ",")
+  parameters <- cbind.data.frame(hs_names, parameters)
+  tibble::as_data_frame(parameters)
+  
+}
+
--- R/phy_struct.R
+++ R/phy_struct.R
@@ -33,61 +33,22 @@
 #' res <- phy_struct(tree)
 #' 
 phy_struct <- function(tree) {
-  # Perform checks
-  if(class(tree) != "phylo") stop("'tree' argument must be class phylo.")
- 
-  # If root has a length
-  root_ancestor = 0
-  long_root <- ifelse(!is.null(tree$root.edge), TRUE, FALSE)
-  
-  # Ancestral species
-  ancestral_nodes <- lapply(as.list(seq_along(tree$tip.label)), function(x) {
-    res <- c(x, phangorn::Ancestors(tree, x, 'all'))
-    if(long_root) c(res, root_ancestor) else res
-  })
-  
-  root_node <- ape::Ntip(tree) + 1
-    
-  if(long_root) {
-    tree$edge <- rbind(tree$edge, c(0, root_node))
-    tree$edge.length <- c(tree$edge.length, tree$root.edge)
-  }
-  
-  # Historic species data for each present day species
-  pds_subset <- lapply(seq_along(ancestral_nodes), function(x) {
-    daughters <- ancestral_nodes[[x]]
-    if(long_root) daughters <- c(daughters, 0)
-    daughters <- daughters[-length(daughters)]
-    res <- lapply(as.list(daughters), function(y) tree$edge[tree$edge[,2] %in% y,])
-    res <- do.call(rbind.data.frame, res)
-    res <- cbind.data.frame(tree$tip.label[x], x, res)
-    colnames(res) <- c("tip_label", "tip_node", "a_node", "d_node")
-    lengths <- sapply(res$d_node, function(x) which(tree$edge[,2] %in% x))
-    lengths <- tree$edge.length[lengths]
-    cbind.data.frame(res, lengths)
-  })
-  pds_subset <- do.call(rbind.data.frame, pds_subset)
-  
-  # Historic species names
-  hs_names <- paste(pds_subset$tip_node, 
-                    paste(pds_subset$a_node, 
-                          pds_subset$d_node, sep = "-"), sep = ",")
-  pds_subset <- cbind.data.frame(hs_names, pds_subset)
-  pds_subset <- tibble::as_data_frame(pds_subset)
+  # Extract parameters associated with each historic species
+  parameters <- hs_parameters(tree)
   
   # Create structural matrix
   s_matrix <- matrix(0, ncol = length(seq_along(tree$tip.label)), 
-                     nrow = nrow(pds_subset))
-  row.names(s_matrix) <- pds_subset$hs_names
+                     nrow = nrow(parameters))
+  row.names(s_matrix) <- parameters$hs_names
   colnames(s_matrix) <- tree$tip.label
-  index <- sapply(pds_subset$tip_label, function(x) which(colnames(s_matrix) %in% x))
-  index <- cbind(row = 1:nrow(pds_subset), col = index)
-  s_matrix[index] <- pds_subset$lengths
+  index <- sapply(parameters$tip_label, function(x) which(colnames(s_matrix) %in% x))
+  index <- cbind(row = 1:nrow(parameters), col = index)
+  s_matrix[index] <- parameters$lengths
   
   # Output
   new('phy_struct', 
       structure = s_matrix, 
-      parameters = pds_subset)
+      parameters = parameters)
 }
 
 
