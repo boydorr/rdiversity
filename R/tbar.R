6742554a62057b4419473fcaa285e9a055211bae
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-03-14 12:00:24 +0000

    new function to calculate tbar

---
+++ R/tbar.R
@@ -0,0 +1,18 @@
+#' tbars
+#' 
+#' Function to calculate T_bar.
+#' 
+#' @param ps \code{phy_struct()} output.
+#' @param partition two-dimensinal \code{matrix} of mode \code{numeric} with
+#' rows as types, columns as subcommunities, and elements containing relative
+#' abundances of types in subcommunities. In the case of phylogenetic
+#' metacommunities, these are the relative abundances of terminal taxa.
+#' @export
+#' 
+tbar <- function(ps, partition) {
+  # Identify which species are here
+  structure <- ps$structure
+  partition <- partition[which(row.names(partition) %in% colnames(structure)),]
+  
+  sum(structure %*% partition)
+}
