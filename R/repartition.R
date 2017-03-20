e7877d95d51c5497596ab5975df55be48330309d
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-03-20 12:25:18 +0000

    repartition: allow vector of abundances to be input (as well as random reshuffling)

--- R/repartition.R
+++ R/repartition.R
@@ -1,18 +1,21 @@
 #' Repartition metacommunity
-#' 
-#' Randomly reshuffles the relative abundance of types (e.g. species) in a 
+#'
+#' Randomly reshuffles the relative abundance of types (e.g. species) in a
 #' metacommunity (whilst maintaining the relationship between the relative
 #' abundance of a particular species across subcommunities). In the case of a
-#' phylogenetic metacommunity, the relative abundance of terminal taxa are 
-#' randomly reshuffled and the relative abundance of types (historic species) 
-#' are calculated from the resulting partition. 
-#' 
+#' phylogenetic metacommunity, the relative abundance of terminal taxa are
+#' randomly reshuffled and the relative abundance of types (historic species)
+#' are calculated from the resulting partition.
+#'
 #' @param meta object of class \code{metacommunity}.
-#' @param ps \code{phy_struct()} output.
-#' 
+#' @param new_partition proportional abundance of \emph{types} in the  
+#' subcommunity as a fraction of the metacommunity as a whole (in the 
+#' phylogenetic case, this corresponds to the proportional abundance of 
+#' terminal taxa)
+#'
 #' @return Returns an object of class \code{metacommunity.}
 #' @export
-#' 
+#'
 #' @examples
 #' tree <- ape::rtree(n = 5)
 #' tree$tip.label <- paste0("sp", seq_along(tree$tip.label))
@@ -21,15 +24,15 @@
 #' partition <- partition / sum(partition)
 #' meta <- metacommunity(partition, tree)
 #' ps <- phy_struct(tree)
-#' 
-#' repartition(meta, ps)
-#' 
-repartition <- function(meta, ps) {
-  
+#'
+#'
+repartition <- function(meta, new_partition) {
+
   if(isTRUE(all.equal(0, length(meta@raw_structure)))) {
     # Non-phylogenetic metacommunity
     partition <- meta@type_abundance
-    new_partition <- partition[sample(seq_along(row.names(partition))),]
+    if (missing(new_partition))
+      new_partition <- partition[sample(seq_along(row.names(partition))),]
     new_partition <- check_partition(new_partition)
     row.names(new_partition) <- row.names(partition)
 
@@ -38,13 +41,14 @@ repartition <- function(meta, ps) {
   }else {
     # Phylogenetic metacommunity
     raw_abundance <- meta@raw_abundance
-    new_abundance <- raw_abundance[sample(seq_along(row.names(raw_abundance))),]
+    if (missing(new_partition))
+      new_abundance <- raw_abundance[sample(seq_along(row.names(raw_abundance))),]
     new_abundance <- check_partition(new_abundance)
     row.names(new_abundance) <- row.names(raw_abundance)
 
     hs_abundance <- phy_abundance(new_abundance, meta@raw_structure)
 
-    new_meta <- metacommunity(hs_abundance, z)
+    new_meta <- metacommunity(hs_abundance, meta@similarity)
     new_meta@raw_abundance <- new_abundance
     new_meta@raw_structure <- meta@raw_structure
     new_meta@parameters <- meta@parameters
