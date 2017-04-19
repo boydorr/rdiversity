fd48b750a4a9824de8db6c4a927c33db5d7e0c4a
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-04-19 09:34:51 +0100

    power_mean.R: remove na.rm’s

--- R/power_mean.R
+++ R/power_mean.R
@@ -26,10 +26,8 @@ function(values, order = 1, weights = rep(1, length(values)))
   stopifnot(length(values) == length(weights))
   
   # Values and weights must be greater than zero
-  if(any(values < 0, na.rm = TRUE))
-    stop('values must be greater than 0')
-  if(any(weights < 0, na.rm = TRUE))
-    stop('weights must be greater than 0')
+  if(any(values < 0)) stop('values must be greater than 0')
+  if(any(weights < 0)) stop('weights must be greater than 0')
   
   # Normalise weights to sum to 1 (as per Rényi)
   proportions <- weights / sum(weights)
@@ -46,13 +44,13 @@ function(values, order = 1, weights = rep(1, length(values)))
   proportions <- proportions[non.zero]
   
   if (abs(order) < .Machine$double.eps ^ 0.5) {  # Avoid rounding errors for order 0
-      prod(values ^ proportions, na.rm = T)
+      prod(values ^ proportions)
     } else if (is.infinite(order)) {
       if (order > 0)
-        max(values, na.rm = T)
+        max(values)
       else
-        min(values, na.rm = T)
+        min(values)
     } else {
-      sum(proportions * values ^ order, na.rm = T) ^ (1 / order)
+      sum(proportions * values ^ order) ^ (1 / order)
     }
 }
