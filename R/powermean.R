01bdd3faaef4bd380baf09952b5d57843103f053
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-04-23 17:21:51 +0100

    powermean.R: store ‘tag’ (measure name)

--- R/powermean.R
+++ R/powermean.R
@@ -4,15 +4,13 @@
 #' into a \code{powermean}; for \code{raw_alpha()}, \code{norm_alpha()}, 
 #' \code{raw_rho()}, \code{norm_rho()}, or  \code{raw_gamma()}. 
 #' 
+#' @param tag measure
 #' @param results \code{matrix} of mode \code{numeric}; contains values 
 #' calculated from diversity-term functions \code{norm_alpha()}, 
 #' \code{raw_alpha()}, \code{raw_rho()}, \code{norm_rho()}, and 
 #' \code{raw_gamma()}
 #' @param meta object of class \code{metacommunity}; contains proportional
 #' abundance of types, pair-wise similarity, and other associated variables.
-#' @param tag object of class \code{character}; contains an identifier
-#' associated with the calculated diversity-term, e.g.
-#' "norm_alpha", "raw_beta", etc.
 #'
 #' @return \code{powermean(x)} returns an object of class \code{powermean}.
 #' @include class-powermean.R
@@ -31,6 +29,7 @@
 powermean <- function(results, meta, tag) {
   new('powermean',
       results = results,
+      measure = tag,
       type_abundance = meta@type_abundance,
       ordinariness = meta@ordinariness,
       subcommunity_weights = meta@subcommunity_weights,
