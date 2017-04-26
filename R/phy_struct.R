7f14f9dd251b938ed4e0090935782866328444a1
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-04-26 19:58:53 +0100

    phy_struct(): require partition as argument for tbar.

--- R/phy_struct.R
+++ R/phy_struct.R
@@ -32,10 +32,13 @@
 #' @examples
 #' tree <- ape::rtree(n = 5)
 #' tree$tip.label <- paste0("sp", seq_along(tree$tip.label))
-#' 
-#' res <- phy_struct(tree)
-#' 
-phy_struct <- function(tree) {
+#' partition <- cbind(a = c(1,1,1,0,0), b = c(0,1,0,1,1))
+#' row.names(partition) <- tree$tip.label
+#' partition <- partition / sum(partition)
+#'
+#' res <- phy_struct(tree, partition)
+#'
+phy_struct <- function(tree, partition) {
   # Extract parameters associated with each historic species
   parameters <- hs_parameters(tree)
   
