51ae0e39a261cc5adc290b224ace326a69a218e3
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-04-26 19:55:41 +0100

    phy_struct(): divide structure matrix output by tbar.

--- R/phy_struct.R
+++ R/phy_struct.R
@@ -1,13 +1,13 @@
 #' Calculate phylogenetic structure matrix
 #'
 #' Converts an object into class \code{phylo} into class \code{phy_struct}.
-#' 
+#'
 #' @param tree object of class \code{phylo}
 
 #' @return Returns a \code{list} containing:
 #' \tabular{ll}{
-#' \code{$structure} \tab - each row denotes historic species, columns denote 
-#' terminal taxa, and elements contain branch lengths \cr
+#' \code{$structure} \tab - each row denotes historic species, columns denote
+#' terminal taxa, and elements contain 'branch lengths / tbar' \cr
 #' \code{$parameters} \tab - information associated with each historic species \cr
 #' \code{$tree} \tab - object of class \code{phylo} \cr
 #' }
@@ -36,13 +36,16 @@ phy_struct <- function(tree) {
   parameters <- hs_parameters(tree)
   
   # Create structural matrix
-  s_matrix <- matrix(0, ncol = length(seq_along(tree$tip.label)), 
+  s_matrix <- matrix(0, ncol = length(seq_along(tree$tip.label)),
                      nrow = nrow(parameters))
   row.names(s_matrix) <- parameters$hs_names
   colnames(s_matrix) <- tree$tip.label
   index <- sapply(parameters$tip_label, function(x) which(colnames(s_matrix) %in% x))
   index <- cbind(row = 1:nrow(parameters), col = index)
   s_matrix[index] <- parameters$lengths
+  T_bar <-  sum(s_matrix %*% partition)
+  
+  s_matrix <- s_matrix / T_bar
   
   # Output
   list(structure = s_matrix, 
