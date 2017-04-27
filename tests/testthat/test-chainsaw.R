1063d804204ea4296207ad230ae54d65f8730d84
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-04-27 10:41:44 +0100

    delete duplicated test.

--- tests/testthat/test-chainsaw.R
+++ tests/testthat/test-chainsaw.R
@@ -14,19 +14,6 @@ test_that("Setting interval to 2 returns a long root for ultrametric trees", {
 })
 
 
-test_that("Setting interval to 2 returns a long root for non-ultrametric trees", {
-  tree <- ape::read.tree(text="(A:1,B:2);")
-  partition <- setNames(c(0.6, 0.4), tree$tip.label)
-  meta <- metacommunity(partition, tree, 2)
-  
-  tree2 <- ape::read.tree(text="(A:1,B:2)R:2;")
-  meta2 <- metacommunity(partition, tree2)
-  
-  expect_equivalent(norm_meta_alpha(meta, 0:2),
-                    norm_meta_alpha(meta2, 0:2))
-})
-
-
 
 test_that("Setting interval to 0.5 cuts off root", {
   tree <- ape::read.tree(text="(A:1,B:2);")
