20a04fc2f874fb26e1ddc9d6976606c6ab9b58e5
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-05-30 15:21:00 +0100

    test-check_partition.R: edit object names.

--- tests/testthat/test-check_partition.R
+++ tests/testthat/test-check_partition.R
@@ -1,11 +1,11 @@
 context('Testing check_partition()')
 
 test_that("The function check_partition() is correct", {
-  pop.vec <- data.frame(a = 1, b = 3)
-  pop.df <- data.frame(a = 1:2, b = 3:4)
+  pop_vec <- data.frame(a = 1, b = 3)
+  pop_df <- data.frame(a = 1:2, b = 3:4)
   
-  expect_warning(pv <- check_partition(pop.vec))
-  expect_warning(pd <- check_partition(pop.df))
+  expect_warning(pv <- check_partition(pop_vec))
+  expect_warning(pd <- check_partition(pop_df))
   expect_equal(class(pd), class(pv))
 })
 
