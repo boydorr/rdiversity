6869e3540e11efe08d1fcdbc8beecb283a4840cf
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-05-30 15:20:48 +0100

    test-check_partition.R: if partition is entered as a matrix with a single species, ensure dimensions remain the same.

--- tests/testthat/test-check_partition.R
+++ tests/testthat/test-check_partition.R
@@ -9,6 +9,14 @@ test_that("The function check_partition() is correct", {
   expect_equal(class(pd), class(pv))
 })
 
+test_that("check_partition() returns the same population dimensions when only a single species is present", {
+  single_sp <- matrix(c(1,3), nrow=1)
+  colnames(single_sp) <- c("a", "b")
+  rownames(single_sp) <- "sp1"
+    
+  expect_warning(expect_equal(dim(check_partition(single_sp)), dim(single_sp)))
+})
+
 test_that("check_partition() returns the same population dimensions when only a single subcommunity is present", {
   single_sc <- matrix(c(1,3), ncol=1)
   rownames(single_sc) <- c("sp1", "sp2")
