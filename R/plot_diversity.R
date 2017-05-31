17522c07d4a8b5953d1b9e5e843845c4370f9c5c
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2017-05-31 12:57:41 +0100

    plot_diversity.R: fix plotting function for a single population.

--- R/plot_diversity.R
+++ R/plot_diversity.R
@@ -1,50 +1,57 @@
 #' Plot diversity
 #'
 #' Simple function to plot diversity profiles.
-#' 
+#'
 #' @param data output
-#' 
+#'
 #' @export
-#' 
-#' @examples 
+#'
+#' @examples
 #' # Define metacommunity
 #' pop <- data.frame(a = c(1,3), b = c(1,1))
 #' row.names(pop) <- paste0("sp", 1:2)
 #' pop <- pop/sum(pop)
 #' meta <- metacommunity(pop)
-#' 
+#'
 #' # Calculate subcommunity beta diversity (takes the relative entropy)
 #' b <- raw_beta(meta)
 #' res <- subdiv(b, 0:2)
 #' plot_diversity(res)
-#' 
+#'
 #' # Calculate all measures of subcommunity diversity
 #' res <- subdiv(meta, 0:2)
 #' plot_diversity(res)
-#' 
+#'
+#' # Try a single population
+#' pop <- c(1,3,4)
+#' meta <- metacommunity(pop)
+#' res <- norm_sub_alpha(meta, 0:2)
+#' plot_diversity(res)
+#'
 plot_diversity <- function(data) {
   community <- unique(data$partition_level)
   measure <- unique(data$measure)
-  
-  if(isTRUE(all.equal(1, length(measure)))) 
-    tag <- get_title(community, measure, TRUE) else 
+  data$partition_name <- as.factor(data$partition_name)
+
+  if(isTRUE(all.equal(1, length(measure))))
+    tag <- rdiversity::get_title(community, measure, TRUE) else
       tag <- paste(community, "diversity")
-  
+
   community <- gsub("subcommunity", "Subcommunity", community)
   community <- gsub("metacommunity", "Metacommunity", community)
-  
-  g <- ggplot2::ggplot(data, ggplot2::aes_string(x="q", y="diversity")) + 
+
+  g <- ggplot2::ggplot(data, ggplot2::aes_string(x="q", y="diversity")) +
     ggplot2::theme_bw() +
     ggplot2::geom_line(ggplot2::aes_string(group = "partition_name",
-                                           colour = "partition_name")) + 
-    ggplot2::labs(x = bquote(italic("q")), y = tag, colour = "Partitions") + 
+                                           colour = "partition_name")) +
+    ggplot2::labs(x = bquote(italic("q")), y = tag, colour = "Partitions") +
     ggthemes::scale_color_ptol()
-  
+
   if(length(measure)>1) {
     x = ggplot2::label_bquote(.(measure))
     g <- g + ggplot2::facet_wrap(~measure, labeller = x)
   }
-  
+
   g
 }
 
