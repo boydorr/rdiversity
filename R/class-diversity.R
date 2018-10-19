cd39eb6361b565b26719894d280a6dda8043a5a2
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-10-19 18:22:30 +0100

    class-diversity.R: Generate new class type `diversity`

---
+++ R/class-diversity.R
@@ -0,0 +1,12 @@
+#' diversity-class
+#'
+#' Container for class \code{diversity}.
+#'
+#' @name diversity-class
+#' @rdname diversity-class
+#' @export diversity
+#'
+setClass("diversity", contains = "data.frame")
+
+
+
