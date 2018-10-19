fe4579799df42ee9a54f87bbb1d34eca6018aa91
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-10-19 18:54:24 +0100

    phy2branch.R: function to convert phylo object to similarity object

---
+++ R/phy2branch.R
@@ -0,0 +1,16 @@
+#' phy2branch
+#' 
+#' Packages phylo object and depth into an object of class \code{similarity}. 
+#' 
+#' @param phylo object of class \code{phylo}.
+#' @param depth proportion of total tree height to be conserved (taken as
+#' a proportion from the heighest tip). Describes how much evolutionary history 
+#' should be retained, with 0 marking the date of the most recent tip, and 1 
+#' (the default) marking the most recent common ancestor. Numbers greater than 
+#' 1 extend the root of the tree.
+#' @return \code{phy2sim(x)} returns an object of class \code{similarity}.
+#' @export
+#' 
+phy2branch <- function(phylo, depth = 1) {
+  similarity(phylo, depth)
+}
