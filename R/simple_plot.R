980a5ef7402d307b83afd0f1f85cf9fb5a1e5a7c
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-10-19 19:01:37 +0100

    simple_plot.R: set lower limit on the y-axis to 0

--- R/simple_plot.R
+++ R/simple_plot.R
@@ -87,12 +87,15 @@ simple_plot <- function(res) {
   names(cols) <- partitions
   res$partition_name <- factor(res$partition_name, levels = partitions)
   
+  ylabs <- pretty(c(0, res$diversity))
+  
   g <- ggplot2::ggplot(res) + ggplot2::theme_bw() +
     ggplot2::geom_line(ggplot2::aes_string(x = "q",
                                            y = "diversity",
                                            group = "partition_name",
                                            colour = "partition_name"),
                         size = 0.5) +
+    ggplot2::scale_y_continuous(breaks = ylabs) +
     ggplot2::scale_color_manual(values = cols) +
     ggplot2::labs(x = bquote(italic("q")), y = "Diversity",
                   colour = "Partition") +
