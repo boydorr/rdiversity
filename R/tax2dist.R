7b8c62d543ede37384c698757f357f2afb575a86
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-10-23 15:54:29 +0100

    Remove erroneous comma in tax2dist documentation.

--- R/tax2dist.R
+++ R/tax2dist.R
@@ -24,7 +24,7 @@
 #' # Create Lookup table
 #' Species <- c("tenuifolium", "asterolepis", "simplex var.grandiflora", "simplex var.ochnacea")
 #' Genus <- c("Protium", "Quararibea", "Swartzia", "Swartzia")
-#' Family <- c("Burseraceae", "Bombacaceae", "Fabaceae", "Fabaceae"),
+#' Family <- c("Burseraceae", "Bombacaceae", "Fabaceae", "Fabaceae")
 #' Subclass <- c("Sapindales", "Malvales", "Fabales", "Fabales")
 #' lookup <- cbind.data.frame(Species, Genus, Family, Subclass)
 #' 
