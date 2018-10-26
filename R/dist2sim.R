2b13c1bf30657ba58ac5f389cd669add5118f01a
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-10-26 16:03:44 +0100

    dist2sim.R: edits to transformations.

--- R/dist2sim.R
+++ R/dist2sim.R
@@ -5,7 +5,7 @@
 #' @param dist two-dimensional \code{matrix} of mode \code{numeric} with rows as 
 #' types, columns as types, and elements containing the pairwise distance 
 #' between types. 
-#' @param transform can be either "l", "e1", or "e2".
+#' @param transform can be either "linear", "exponential".
 #' @param k scaling parameter.
 #' @param normalise normalise distances to one; can be either true of false.
 #' @param max_d object of mode \code{numeric}; 
@@ -19,11 +19,12 @@
 #' dist2sim(dist, "l")
 #' 
 #' 
-dist2sim <- function(dist, transform, k = 1, normalise = TRUE, max_d = 1) {
-  if(normalise) dist <- dist/max(dist)
+dist2sim <- function(dist, transform, k = 1,
+                     normalise = TRUE, max_d = max(dist)) {
+  if(normalise) dist <- dist/max_d
   
-  if(transform == "l") return(pmax(1-(dist/(max_d*k)), 0))
-  if(transform == "e1") return(exp(-(dist*k)))
-  if(transform == "e2") return(exp(-(dist/(max(dist)*k))))
-
+  if(transform == substr("linear", 1, nchar(transform)))
+    return(pmax(1 - k * dist, 0))
+  if(transform == substr("exponential", 1, nchar(transform)))
+    return(exp(-k * dist))
 }
