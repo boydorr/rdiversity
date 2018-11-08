837b4cec4057a1e89872fa72fce59c31789469e5
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-11-08 18:13:10 +0000

    distance.R: add slots to store objects required to calculate taxonomic distance.

--- R/class-distance.R
+++ R/class-distance.R
@@ -6,12 +6,16 @@
 #' @rdname distance-class
 #' @exportClass distance
 #' 
-#' @field phylo object of class \code{phylo}.
-#' @field depth object of class \code{numeric}.
+#' @field distance object of class \code{matrix}.
+#' @field divID object of class \code{character}.
+#' @field values object of class \code{numeric}.
+#' @field taxID object of class \code{numeric}.
+#' @field taxMask object of class \code{list}.
 #' 
 setClass("distance", slots = c(distance = "matrix",
                                divID = "character",
+                               values = "numeric",
                                taxID = "numeric",
-                               taxmask = "list"))
+                               taxMask = "list"))
 
 
--- R/distance.R
+++ R/distance.R
@@ -2,13 +2,38 @@
 #' 
 #' distance
 #' 
-#' @param depth object of class \code{numeric}
+#' @param distance lksdjf
+#' @param divID lskdj
+#' @param values lskdj
+#' @param taxID lskdj
+#' @param taxMask lskdj
 #' 
 #' @return distance() returns an object of class \code{distance}
 #' 
-distance <- function(distance, what) {
+distance <- function(distance, divID, values, taxID, taxMask) {
   new('distance', 
       distance = distance, 
-      what = what)
+      divID = divID,
+      values = values,
+      taxID = taxID, 
+      taxMask = taxMask)
 }
 
+
+
+#' @rdname distance-class
+#' @param object object of class \code{distance}
+#'
+setMethod(f = "show", signature = "distance",
+          definition = function(object) {
+            cat('Object of class distance, containing:\n')
+            cat('@distance: Matrix of pairwise distances (', 
+                ncol(object@distance), 'types\n')
+            cat('@divID: Identifier describing the type of data being analysed\n')
+            
+            if(!isTRUE(all.equal(0, length(object@taxID))))
+            cat('@values: Vector of ...\n')
+            cat('@taxID: Vector of ...\n')
+            cat('@taxMask: ListVector of ...\n')
+            
+          } )
