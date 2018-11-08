d58cab190c039de1b39ab8e77974ff576ab5e926
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-11-08 17:42:13 +0000

    taxid.R: initial commit of function and documentation

---
+++ R/taxid.R
@@ -0,0 +1,23 @@
+#' taxid
+#' 
+#' Generate taxanomic codes for each species by converting species, genus, 
+#' family, and subclass into factors
+#' 
+#' @param lookup
+#' 
+taxid <- function(lookup) {
+  N <- apply(lookup, 2, function(x) length(unique(x)))
+  bits <- round(log(N, 2))
+  df <- taxfac(lookup)
+  
+  output <- lapply(seq_along(bits), function(x) {
+    tmp <- df[x,]
+    tmp <- lapply(seq_along(tmp), function(y) 
+      binaryLogic::as.binary(tmp[y], n = bits[y])) # intToBits(x)
+    tmp <- unlist(tmp)  
+    tmp <- binaryLogic::as.binary(tmp, logic = TRUE)
+    sum(2^(which(rev(unlist(strsplit(as.character(tmp), "")) == 1))-1))
+  })
+  names(output) <- lookup[,1]
+  output
+}
