06d2cd1cb7395303e6516be2ee9d7733db40ed36
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-11-15 19:30:25 +0000

    taxid.R: take taxFac as input rather than lookup.

--- R/taxid.R
+++ R/taxid.R
@@ -3,22 +3,21 @@
 #' Generate taxanomic codes for each species by converting species, genus, 
 #' family, and subclass into factors
 #' 
-#' @param lookup \code{data.frame} with colnames corresponding to nested 
-#' hierarchical levels, e.g. c('Species', 'Genus', 'Family', 'Subclass')
+#' @param taxFac sdf
 #' 
-taxid <- function(lookup) {
-  N <- apply(lookup, 2, function(x) length(unique(x)))
-  bits <- round(log(N, 2))
-  df <- taxfac(lookup)
-  
-  output <- lapply(seq_along(bits), function(x) {
-    tmp <- df[x,]
+taxid <- function(taxFac) {
+  # taxFac <- taxFac + 1
+  species <- row.names(taxFac)
+  bits <- apply(taxFac, 2, function(x) ceiling(log(max(x)+1, 2)))
+
+  output <- lapply(seq_along(species), function(x) {
+    tmp <- taxFac[x,]
     tmp <- lapply(seq_along(tmp), function(y) 
       binaryLogic::as.binary(tmp[y], n = bits[y])) # intToBits(x)
     tmp <- unlist(tmp)  
     tmp <- binaryLogic::as.binary(tmp, logic = TRUE)
     sum(2^(which(rev(unlist(strsplit(as.character(tmp), "")) == 1))-1))
   })
-  names(output) <- lookup[,1]
+  names(output) <- row.names(taxFac)
   unlist(output)
 }
