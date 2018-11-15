f74ddded85bc8a8b4a5f5a4ccfb895e18af22259
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-11-15 19:27:46 +0000

    distance.R: remove distance function.

--- R/distance.R
+++ R/distance.R
@@ -1,26 +1,3 @@
-#' Generate distance object
-#' 
-#' distance
-#' 
-#' @param distance lksdjf
-#' @param divID lskdj
-#' @param values lskdj
-#' @param taxID lskdj
-#' @param taxMask lskdj
-#' 
-#' @return distance() returns an object of class \code{distance}
-#' 
-distance <- function(distance, divID, values, taxID, taxMask) {
-  new('distance', 
-      distance = distance, 
-      divID = divID,
-      values = values,
-      taxID = taxID, 
-      taxMask = taxMask)
-}
-
-
-
 #' @rdname distance-class
 #' @param object object of class \code{distance}
 #'
@@ -28,12 +5,12 @@ setMethod(f = "show", signature = "distance",
           definition = function(object) {
             cat('Object of class distance, containing:\n')
             cat('@distance: Matrix of pairwise distances (', 
-                ncol(object@distance), 'types\n')
-            cat('@divID: Identifier describing the type of data being analysed\n')
+                ncol(object@distance), 'types )\n')
+            cat('@datID:', object@datID, '\n')
             
             if(!isTRUE(all.equal(0, length(object@taxID))))
-            cat('@values: Vector of ...\n')
+            cat('@taxDistance: Vector of ...\n')
             cat('@taxID: Vector of ...\n')
             cat('@taxMask: ListVector of ...\n')
-            
+            cat('@taxBits: ListVector of ...\n')
           } )
