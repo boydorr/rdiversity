7ae6ce982b112504c66bb1e2593eed1663880050
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-11-16 16:34:51 +0000

    distance.R: change the way distance objects are printed.

--- R/distance.R
+++ R/distance.R
@@ -3,14 +3,5 @@
 #'
 setMethod(f = "show", signature = "distance",
           definition = function(object) {
-            cat('Object of class distance, containing:\n')
-            cat('@distance: Matrix of pairwise distances (', 
-                ncol(object@distance), 'types )\n')
-            cat('@datID:', object@datID, '\n')
-            
-            if(!isTRUE(all.equal(0, length(object@taxID))))
-            cat('@taxDistance: Vector of ...\n')
-            cat('@taxID: Vector of ...\n')
-            cat('@taxMask: ListVector of ...\n')
-            cat('@taxBits: ListVector of ...\n')
+            cat('Object of class distance, containing either: (1) a distance matrix; or (2) all of the data required to calculate a distance matrix.')
           } )
