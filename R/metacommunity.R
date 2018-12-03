0aa9899f02b085522eb28913abee32db88301b0e
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-12-03 12:37:46 +0000

    edit documentation.

--- R/metacommunity.R
+++ R/metacommunity.R
@@ -208,7 +208,22 @@ setMethod(f = "metacommunity",
 
 #' @rdname metacommunity-methods
 #' @aliases metacommunity,similarity-method
-#'
+#' 
+#' @examples 
+#' # Create Lookup table
+#' Species <- c("tenuifolium", "asterolepis", "simplex var.grandiflora", "simplex var.ochnacea")
+#' Genus <- c("Protium", "Quararibea", "Swartzia", "Swartzia")
+#' Family <- c("Burseraceae", "Bombacaceae", "Fabaceae", "Fabaceae")
+#' Subclass <- c("Sapindales", "Malvales", "Fabales", "Fabales")
+#' lookup <- cbind.data.frame(Species, Genus, Family, Subclass)
+#' 
+#' # Assign values for each level (Shimatani's taxonomic distance)
+#' taxDistance <- c(Species = 0, Genus = 1, Family = 2, Subclass = 3, Other = 4)
+#' 
+#' # Generate pairwise distances
+#' distance <- tax2dist(lookup, taxDistance, FALSE)
+#' similarity <- dist2sim(distance, "linear")
+#' 
 setMethod(f = "metacommunity",
           signature(partition = "matrix", similarity = "similarity"),
           definition = function(partition, similarity) {
