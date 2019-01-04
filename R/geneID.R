9eb7cae1811a756a00891495a920e65ecd326532
mysteryduck <sonia.mitchell@glasgow.ac.uk>
2019-01-04 15:43:46 +0000

    geneID.R: initial commit

---
+++ R/geneID.R
@@ -0,0 +1,24 @@
+#' geneID
+#' 
+#' Converts a single sequence
+#' 
+#' @param seq a single read
+#' @param kmer is a 16-mer by default
+#' 
+geneID <- function(seq, kmer = 16) {
+  dat <- as.numeric(as.factor(seq)) - 1
+  
+  ind <- data.frame(start = seq(1, length(dat) - kmer + 1, 1), 
+                    end = seq(kmer, length(dat), 1))
+  
+  output <- sapply(1:nrow(ind), function(x) {
+    tmp <- dat[ind$start[x]:ind$end[x]]
+    tmp <- lapply(seq_along(tmp), function(y) 
+      binaryLogic::as.binary(tmp[y], n = 2)) 
+    tmp <- unlist(tmp)  
+    tmp <- binaryLogic::as.binary(tmp, logic = TRUE)
+    sum(2^(which(rev(unlist(strsplit(as.character(tmp), "")) == 1))-1))
+  })
+  
+  output <- sort(output)
+}
