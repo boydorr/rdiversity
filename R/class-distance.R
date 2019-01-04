384510ba25e6b55cd86a348a305b45234045d33d
mysteryduck <sonia.mitchell@glasgow.ac.uk>
2019-01-04 15:28:05 +0000

    class-distance.R: combine slots in distance class.

--- R/class-distance.R
+++ R/class-distance.R
@@ -6,24 +6,12 @@
 #' @rdname distance-class
 #' @exportClass distance
 #' 
-#' @field similarity object of class \code{matrix}.
+#' @field distance object of class \code{matrix}.
 #' @field datID object of class \code{character}.
-#' @field ordinariness object of class \code{character}.
-#' @field taxDistance object of class \code{numeric}.
-#' @field taxID object of class \code{numeric}.
-#' @field taxMask object of class \code{list}.
-#' @field taxBits object of class \code{numeric}.
-#' @field tree object of class \code{data.frame}.
-#' @field treeDepth object of class \code{numeric}.
+#' @field components object of class \code{list}.
 #' 
 setClass("distance", slots = c(distance = "matrix",
                                datID = "character",
-                               ordinariness = "character",
-                               taxDistance = "numeric",
-                               taxID = "numeric", 
-                               taxMask = "list",
-                               taxBits = "numeric",
-                               tree = "data.frame", 
-                               treeDepth = "numeric"))
+                               components = "list"))
 
 
