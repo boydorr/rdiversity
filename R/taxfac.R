ae98d96d619ad5993cbe421f6e1f90db5afd9840
mysteryduck <sonia.mitchell@glasgow.ac.uk>
2019-01-04 16:24:11 +0000

    taxfac.R: minor edits

--- R/taxfac.R
+++ R/taxfac.R
@@ -16,8 +16,9 @@
 #' }
 #' 
 taxfac <- function(lookup) {
-  output <- lookup
-  for(i in 1:ncol(output)) output[,i] <- as.numeric(as.factor(output[,i]))
+  output <- matrix(ncol = ncol(lookup), nrow = nrow(lookup))
+  for(i in 1:ncol(lookup)) output[,i] <- as.numeric(as.factor(lookup[,i]))
   row.names(output) <- lookup[,1]
+  colnames(output) <- colnames(lookup)
   output - 1
 }
