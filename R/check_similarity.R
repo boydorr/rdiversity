fc72d4ea0eb09ad001fe56f2336c79cff5301165
mysteryduck <sonia.mitchell@glasgow.ac.uk>
2019-01-05 13:02:41 +0000

    check_similarity.R: reorder arguments and remove unused code.

--- R/check_similarity.R
+++ R/check_similarity.R
@@ -2,35 +2,34 @@
 #'
 #' \code{check_similarity()} is used to validate similarity matrices.
 #'
+#' @param similarity two-dimensinal \code{matrix} of mode \code{numeric};
+#' contains pair-wise similarity between types.
 #' @param partition two-dimensinal \code{matrix} of mode \code{numeric} with
 #' rows as types, columns as subcommunities, and elements containing relative
 #' abundances of types in subcommunities. In the case of phylogenetic
 #' metacommunities, these are the relative abundances of terminal taxa.
-#' @param similarity two-dimensinal \code{matrix} of mode \code{numeric};
-#' contains pair-wise similarity between types.
 #'
 #' @return Returns a two-dimensions \code{matrix} of mode \code{numeric}. If
 #' the similarity matrix was valid, this should be identical to that which was
 #' input as an argument.
 #'
-check_similarity <- function(partition, similarity) {
+check_similarity <- function(similarity, partition) {
   if(is.data.frame(similarity)) similarity <- as.matrix(similarity)
-
+  
   if(any(similarity[!is.na(similarity)]<0))
     stop('similarity matrix elements must take positive values.')
-
-  # if(any(similarity[!is.na(similarity)]>1))
-    # warning('non-phylogenetic similarity matrix elements should take a value between 0 and 1.')
-
+  
   if(ncol(similarity)!=nrow(similarity))
     stop('similarity matrix must be square.')
-
-  if(nrow(similarity)!=nrow(partition))
-    stop('similarity and partition matrices must have equal types.')
-
-  if(is.null(row.names(similarity))){
-    row.names(similarity) <- row.names(partition)
-    colnames(similarity) <- row.names(partition)
+  
+  if(!missing(partition)) {
+    if(nrow(similarity)!=nrow(partition))
+      stop('similarity and partition matrices must have equal types.')
+    
+    if(is.null(row.names(similarity))){
+      row.names(similarity) <- row.names(partition)
+      colnames(similarity) <- row.names(partition)
+    }
   }
 
   similarity
