535dbbacd60f8f30ec6940d1a347b5c9da427666
mysteryduck <sonia.mitchell@glasgow.ac.uk>
2019-01-05 12:09:17 +0000

    distance.R: only allow distance matrix and datID to be input.

--- R/distance.R
+++ R/distance.R
@@ -1,3 +1,31 @@
+#' Generate distance object
+#'
+#' Container for class \code{metacommunity}.
+#' 
+#' @param distance distance matrix
+#' @param datID object of class \code{character} denoting the type of diversity 
+#' being calculated. This can be "naive", "genetic", "taxonomic", and so on
+#'
+#' @return \code{distance()} returns an object of class \code{distance}.
+#'
+setGeneric(name = "distance",
+           def = function(distance, datID) {
+             standardGeneric("distance")
+           } )
+          
+
+#' @rdname distance-methods
+#' @aliases distance
+#'
+setMethod(f = "distance",
+          signature(distance = "matrix", datID = "character"),
+          definition = function(distance, datID) {
+            new('distance',
+                distance = distance,
+                datID = datID)
+          } )
+
+
 #' @rdname distance-class
 #' @param object object of class \code{distance}
 #'
@@ -6,41 +34,3 @@ setMethod(f = "show", signature = "distance",
             cat('Object of class `distance`, containing either:\n (1) a distance matrix; or\n (2) all of the data required to calculate a distance matrix.')
           } )
 
-
-#' Generate distance object
-#'
-#' @param distance distance matrix
-#' @param datID 
-#' @param components sdf
-#'
-#' @return \code{distance()} returns an object of class \code{distance}.
-#'
-setGeneric(name = "distance",
-           def = function(distance, datID, components) {
-             standardGeneric("distance")
-           } )
-
-
-#' @rdname distance-methods
-#' @aliases distance
-#'
-setMethod(f = "distance",
-          signature(distance = "matrix", datID = "character", components = "list"),
-          definition = function(distance, datID, components) {
-            new('distance',
-                distance = distance,
-                datID = datID,
-                components = components)
-          } )
-          
-
-#' @rdname distance-methods
-#' @aliases distance
-#'
-setMethod(f = "distance",
-          signature(distance = "matrix", datID = "character", components = "missing"),
-          definition = function(distance, datID, components) {
-            new('distance',
-                distance = distance,
-                datID = datID)
-          } )
